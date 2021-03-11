c7c6651d6db0bbad93a8ef6d20a34b5bc8200c04
otrack <0track@gmail.com>
2021-03-11 13:54:01 +0100

    update

--- java/src/test/java/eu/cloudbutton/dobj/Benchmark.java
+++ java/src/test/java/eu/cloudbutton/dobj/Benchmark.java
@@ -15,12 +15,12 @@ public class Benchmark {
     @Test
     public void counter() throws InterruptedException, ExecutionException {
 
-        final int nbTasks = 10;
-        final int nbThreads = 5;
+        final int nbOps = 100000000;
+        final int nbThreads = Runtime.getRuntime().availableProcessors()/2;
         final int nbTest = 2;
 
         CounterFactory counterFactory = new CounterFactory();
-        Counter count = counterFactory.createdegradablecounter();
+        Counter count = counterFactory.createjavacounter();
 
         List<Double> result = new ArrayList<>();
         Map<Integer, List<Double>> results = new HashMap<>();
@@ -30,8 +30,9 @@ public class Benchmark {
                 List<Callable<Void>> callables = new ArrayList<>();
                 ExecutorService executor = Executors.newFixedThreadPool(i);
 
-                for (int j = 0; j < nbTasks; j++) {
-                    callables.add(new testCallable(count));
+                CountDownLatch latch = new CountDownLatch(i);
+                for (int j = 0; j < i; j++) {
+                    callables.add(new testCallable(count,latch,nbOps/i));
                 }
 
                 double startTime = System.nanoTime();
@@ -41,12 +42,12 @@ public class Benchmark {
                     future.get();
                 }
                 double endTime = System.nanoTime();
-                double duration = (endTime - startTime)/1000000000.0;
-                System.out.println(count.read());
-                System.out.println(duration+" seconds, "+ "nbThreads = "+i+", test num : "+a);
+                double duration = endTime - startTime;
+                System.out.println(count.read()+" operations; "+i+" threads");
+                System.out.println(duration+" time per op: "+ duration/(((double)nbOps)/((double)i))+"ns");
                 result.add(duration);
                 executor.shutdown();
-                count = counterFactory.createdegradablecounter();
+                count = counterFactory.createjavacounter();
             }
             results.put(i, result);
             result = new ArrayList<>();
@@ -59,9 +60,13 @@ public class Benchmark {
     private class testCallable implements Callable<Void> {
 
         private final Counter count;
+        private final CountDownLatch latch;
+        private final int nbOps;
 
-        public testCallable(Counter count) {
+        public testCallable(Counter count, CountDownLatch latch, int nbOps) {
             this.count = count;
+            this.latch = latch;
+            this.nbOps = nbOps;
         }
 
 
@@ -70,9 +75,14 @@ public class Benchmark {
 /*                String pid_name = Thread.currentThread().getName();
                 System.out.println("my_name : "+pid_name);*/
 
-            int nbOp = 10000000;
-            for (int i = 0; i < nbOp; i++) {
-                this.count.write();
+            latch.countDown();
+            try {
+                latch.await();
+                for (int i = 0; i < nbOps; i++) {
+                    this.count.write();
+                }
+            } catch (InterruptedException e) {
+                //ignore
             }
             return null;
         }
