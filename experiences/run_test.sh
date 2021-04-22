85aae084d689a277d1d6ba718dc772cf72781142
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2021-04-22 10:46:11 +0000

    test results for all objects

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,12 +6,12 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 mvn clean package -f ../java -DskipTests;
 
-for type in 'Counter' 'DegradableCounter' 'List' 'DegradableList' 'Set' 'DegradableSet'
+for type in 'Counter' 'DegradableCounter' 'CounterSnapshot' 'List' 'DegradableList' 'ListSnapshot' 'Set' 'DegradableSet' 'SetSnapshot'
 do
   echo $type
   if [ $type = 'Counter'  ] || [ $type = 'DegradableCounter' ]; then
-         CLASSPATH=../java/target/*:../java/target/lib/* java  eu.cloudbutton.dobj.Benchmark -type $type -ratios 100 -nbTest 5 -nbOps 10000000 > "result_${type}.txt"
+         CLASSPATH=../java/target/*:../java/target/lib/* java  eu.cloudbutton.dobj.Benchmark -type $type -ratios 100 -nbTest 5 -nbOps 30000000 > "result_${type}.txt"
   else
-     CLASSPATH=../java/target/*:../java/target/lib/* java  eu.cloudbutton.dobj.Benchmark -type $type -ratios 100 -ow -nbTest 5 -nbOps 1000000 > "result_${type}.txt"
+     CLASSPATH=../java/target/*:../java/target/lib/* java  eu.cloudbutton.dobj.Benchmark -type $type -ratios 100 -ow -nbTest 5 -nbOps 3000000 > "result_${type}.txt"
   fi
 done
