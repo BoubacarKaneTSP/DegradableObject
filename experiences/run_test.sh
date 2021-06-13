7a5184653d4e4651207aee51c99d995e197d395e
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2021-06-13 10:50:50 +0000

    update single reader

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -11,7 +11,7 @@ do
   for type in 'Counter' 'Set' 'List' 'DegradableCounter' 'DegradableSet' 'DegradableList' 'CounterSnapshot' 'SetSnapshot' 'ListSnapshot' 'SecondDegradableList' 'ThirdDegradableList'
   do
     echo $type $ratio
-    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 1 -time 60 -wTime 25 #> "results_${type}_ratio_write_${ratio}.txt"
+    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 1 -time 60 -wTime 25 > "results_${type}_ratio_write_${ratio}_single_reader.txt"
   done
 done
 
