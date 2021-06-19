4150f8db4af4ae29006f129cfcbd3f002bcde87d
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2021-06-19 17:34:54 +0200

    update

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,12 +6,12 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 mvn clean package -f ../java -DskipTests;
 
-for ratio in 95 80 50 20 5;
+for ratio in 95;# 80 50 20 5;
 do
-  for type in 'Counter' 'Set' 'List' 'LinkedList' 'DegradableCounter' 'DegradableSet' 'DegradableList' 'DegradableLinkedList' 'CounterSnapshot' 'SetSnapshot' 'ListSnapshot' 'LinkedListSnapshot' 'CounterSnapshotV2' 'SetSnapshotV2' 'ListSnapshotV2' 'LinkedListSnapshotV2'
+  for type in 'Counter' 'Set' 'List' 'LinkedList' #'DegradableCounter' 'DegradableSet' 'DegradableList' 'DegradableLinkedList' 'CounterSnapshot' 'SetSnapshot' 'ListSnapshot' 'LinkedListSnapshot' 'CounterSnapshotV2' 'SetSnapshotV2' 'ListSnapshotV2' 'LinkedListSnapshotV2'
   do
     echo $type $ratio
-    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 1 -time 60 -wTime 25 > "results_${type}_ratio_write_${ratio}_SR.txt"
+    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 1 -time 60 -wTime 25 # > "results_${type}_ratio_write_${ratio}_SR.txt"
   done
 done
 
--- java/src/test/java/eu/cloudbutton/dobj/Benchmark.java
+++ java/src/test/java/eu/cloudbutton/dobj/Benchmark.java
@@ -129,6 +129,8 @@ public class Benchmark {
                 System.out.println(i + " " + (time) / avg_op); // printing the avg time per op for i thread(s)
                 nbOperations = new ConcurrentLinkedQueue<>();
                 i = 2 * i;
+                if(i==2)
+                    i = nbThreads;
                 if (i > nbThreads && i != 2 * nbThreads) {
                     i = nbThreads;
                 }
