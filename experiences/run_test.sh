06795649a95740bc56aa742fe68619e72a43a625
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2021-06-27 16:58:53 +0200

    update

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,13 +6,14 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 mvn clean package -f ../java -DskipTests;
 
-for ratio in 95 80 50 20 5;
+for ratio in 95; # 80 50 20 5;
 do
-  for type in 'Counter' 'DegradableCounter' 'Set' 'DegradableSet' 'List' 'DegradableList' 'LinkedList' 'DegradableLinkedList' 'CounterSnapshot' 'SetSnapshot' 'ListSnapshot' 'LinkedListSnapshot' 'CounterSnapshotV2' 'SetSnapshotV2' 'ListSnapshotV2' 'LinkedListSnapshotV2'
+  for type in 'Counter' 'DegradableCounter'
+#  for type in 'Counter' 'DegradableCounter' 'Set' 'DegradableSet' 'List' 'DegradableList' 'LinkedList' 'DegradableLinkedList' 'CounterSnapshot' 'SetSnapshot' 'ListSnapshot' 'LinkedListSnapshot' 'CounterSnapshotV2' 'SetSnapshotV2' 'ListSnapshotV2' 'LinkedListSnapshotV2'
 #  for type in 'DegradableCounter'
     do
     echo $type $ratio
-    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 2 -time 60 -wTime 20 > "results_${type}_ratio_write_${ratio}_SR.txt"
+    CLASSPATH=../java/target/*:../java/target/lib/* java eu.cloudbutton.dobj.Benchmark -type $type -ratios $ratio -nbTest 1 -time 90 -wTime 25 > "results_${type}_ratio_write_${ratio}_SR.txt"
   done
 done
 
