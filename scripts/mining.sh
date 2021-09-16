3141f454c9d427dc3df79e0b0752b388bd1720d8
otrack <0track@gmail.com>
2021-09-17 01:11:38 +0200

    fix mining script

--- scripts/mining.sh
+++ scripts/mining.sh
@@ -2,29 +2,33 @@
 
 TMPDIR=/tmp
 
-clazzes="ConcurrentSkipListSet"
-repo="https://github.com/apache/ignite"
+clazzes="ConcurrentSkipListSet ConcurrentLinkedQueue ConcurrentHashMap AtomicLong"
+repo="https://github.com/apache/ignite https://github.com/apache/hadoop https://github.com/apache/cassandra"
 
 for r in ${repo}
 do
     software=$(echo ${r} | awk -F"/" '{print $NF}')
     git clone ${r} ${TMPDIR}/${software}
-    for clazz in ${clazzes}
+    files=$(find ${TMPDIR}/${software} -type f -iname "*.java")
+    for f in ${files}
     do
-	files=$(find ${TMPDIR}/${software} -iname "*.java" | xargs egrep "[a-z]+ = new ${clazz}" $1 | awk '{print $1}' | sed s/://g)
-	for f in ${files}
+	for clazz in ${clazzes}
 	do
-	    var=$(egrep "[a-z]+ = new ${clazz}" $f | awk -F"=" '{print $1}' | awk '{print $NF}' | sed s/this\.//g | xargs echo -n)
-	    isPrivate=$(grep ${var} ${f} | grep private) # FIXME    
-	    if [ "${isPrivate}" != "" ];
-	    then
-		isPrivate="Y"
-	    else
-		isPrivate="N"
+	    hit=$(egrep "[a-z]+ = new ${clazz}" ${f})
+	    if [ "${hit}" != "" ];
+	       then 
+		   var=$(egrep "[a-z]+ = new ${clazz}" $f | awk -F"=" '{print $1}' | awk '{print $NF}' | sed s/this\.//g | head -n 1 | xargs echo -n) # FIXME
+		   isPrivate=$(grep ${var} ${f} | grep private) # FIXME
+		   if [ "${isPrivate}" != "" ];
+		   then
+		       isPrivate="Y"
+		   else
+		       isPrivate="N"
+		   fi
+		   c=$(echo ${f} | awk -F"/" '{print $NF}' | sed s/\\.java//g)
+		   u=$(grep "${var}\." $(find ${TMPDIR}/${software} -iname ${c}.java) | sed "s/.*${var}\.\([a-zA-Z]*\).*/\1/g" | sort | uniq | rs -T)
+		   echo ${software}" "${clazz}" "${c}" "${isPrivate}" "${u}
 	    fi
-	    c=$(echo ${f} | awk -F"/" '{print $NF}' | sed s/\\.java//g)
-	    u=$(grep "${var}\." $(find ${TMPDIR}/${software} -iname ${c}.java) | sed "s/.*${var}\.\([a-zA-Z]*\).*/\1/g" | sort | uniq | rs -T)
-	    echo ${software}" "${clazz}" "${c}" "${isPrivate}" "${u}
 	done
     done
     rm -Rf ${TMPDIR}/${software}
