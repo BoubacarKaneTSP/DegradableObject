172c1c2791fbae648be2cfd36e7858619639d36a
otrack <0track@gmail.com>
2021-10-01 17:43:34 +0200

    mining.sh track return value usage

--- scripts/mining.sh
+++ scripts/mining.sh
@@ -16,23 +16,33 @@ do
 	do
 	    hit=$(egrep "[a-z]+ = new ${clazz}" ${f})
 	    if [ "${hit}" != "" ];
-	       then 
-		   var=$(egrep "[a-z]+ = new ${clazz}" $f | awk -F"=" '{print $1}' | awk '{print $NF}' | sed s/this\.//g | head -n 1 | xargs echo -n) # FIXME
-		   isPrivate=$(grep ${var} ${f} | grep private) # FIXME
-		   if [ "${isPrivate}" != "" ];
-		   then
-		       isPrivate="Y"
-		   else
-		       isPrivate="N"
-		   fi
-		   c=$(echo ${f} | awk -F"/" '{print $NF}' | sed s/\\.java//g)
-		   u=$(grep "${var}\." $(find ${TMPDIR}/${software} -iname ${c}.java) | sed "s/.*${var}\.\([a-zA-Z]*\).*/\1/g" | sort | uniq | rs -T)
-		   echo ${software}" "${clazz}" "${c}" "${isPrivate}" "${u}
+	    then
+		var=$(grep -oP "[[:alpha:]]+ = new ${clazz}" $f | awk -F"=" '{print $1}' | awk '{print $NF}' | head -n 1) # FIXME multiple declaration on the same line ..
+
+		isPrivate=$(grep ${var} ${f} | grep private) # FIXME
+		if [ "${isPrivate}" != "" ];
+		then
+		    isPrivate="Y"
+		else
+		    isPrivate="N"
+		fi
+		c=$(echo ${f} | awk -F"/" '{print $NF}' | sed s/\\.java//g)
+		u=$(grep "${var}\." $(find ${TMPDIR}/${software} -iname ${c}.java) | sed "s/.*${var}\.\([a-zA-Z]*\).*/\1/g" | sort | uniq | rs -T)
+		r=()
+		for m in ${u};
+		do
+		    me=""
+		    if [ "$(find ${TMPDIR}/${software} -iname ${c}.java | xargs grep -P "(return[[:blank:]]*${var}\.${m}\(.*\))|(\=[[:blank:]]*${var}\.${m}\(.*\))|(\(${var}\.${m}\(.*\))" $1)" != "" ];
+		    then
+			me+="+"
+		    fi
+		    me+=${m}
+		    r+=(${me})
+		done
+		echo ${software}" "${clazz}" "${c}" "${isPrivate}" "${r[@]}
 	    fi
 	done
     done
     rm -Rf ${TMPDIR}/${software}
 done
-
-
 
