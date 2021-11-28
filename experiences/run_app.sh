a6ac9ab34d710bcaf0cbbe091a76d385e2230d1a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2021-11-28 13:37:59 +0000

    results degradable objects on noname

--- experiences/run_app.sh
+++ experiences/run_app.sh
@@ -7,7 +7,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 mvn clean package -f ../java -DskipTests;
 
 echo "********** Test objet java concurrent **********"
-CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set Set -list List -counter Counter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
+#CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set Set -list List -counter Counter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
 
 echo "********** Test objet degradable **********"
-#CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set DegradableSet -list MapQueue -counter DegradableCounter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
+CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set DegradableSet -list MapQueue -counter DegradableCounter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
