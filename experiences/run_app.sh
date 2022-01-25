2617b3fdcd105f0db0e77ecc4c7c392d51896e25
Boubacar Kane <boubacar.kane@telecom-sudparis.eu>
2022-01-26 00:14:02 +0100

    results degradable objects

--- experiences/run_app.sh
+++ experiences/run_app.sh
@@ -10,4 +10,4 @@ echo "********** Test objet java concurrent **********"
 #CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set Set -list List -counter Counter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -alphaInit 1.5 -alphaMin 1.2 -alphaStep 0.05 -s -p
 
 echo "********** Test objet degradable **********"
-CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set Set -list MapQueue -counter DegradableCounter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -alphaInit 1.315 -alphaMin 1.315 -alphaStep 0.05 -s -p
+CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.App -set DegradableSet -list MapQueue -counter DegradableCounter -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -alphaInit 1.315 -alphaMin 1.315 -alphaStep 0.05 -s -p
