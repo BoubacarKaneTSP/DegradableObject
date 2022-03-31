952faa3e1e16003222e4d027cca044bccaded6b7
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-03-31 02:04:38 +0200

    microbench

--- experiences/Makeplot.py
+++ experiences/Makeplot.py
@@ -40,6 +40,7 @@ plt.xticks(fontsize=SIZE)
 plt.yticks(fontsize=SIZE)
 
 
+plt.gca().set_ylim([10000,10000000])
 # plt.gca().set_ylim([0.00000001,0.001])
 # plt.gca().invert_yaxis()
 plt.gca().spines['right'].set_visible(False)
