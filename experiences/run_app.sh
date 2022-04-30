6a36d18045b51a27147adac0ba69be52a88cb8f5
Boubacar Kane <boubacar.kane@telecom-sudparis.eu>
2022-04-30 17:00:36 +0200

    update

--- experiences/run_app.sh
+++ experiences/run_app.sh
@@ -7,7 +7,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 mvn clean package -f ../java -DskipTests;
 
 echo "********** Test objet java concurrent **********"
-CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.App -set ConcurrentSkipListSet -queue ConcurrentLinkedQueue -counter Counter -map ConcurrentHashMap -distribution 100 0 0 0 -nbTest 1 -time 10 -wTime 30 -p
+CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.App -set DegradableSet -queue ConcurrentLinkedQueue -counter Counter -map ConcurrentHashMap -distribution 0 0 100 0 -nbTest 1 -time 1 -wTime 3 -p
 
 echo "********** Test objet degradable **********"
-#CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.App -set DegradableSet -queue MapQueue -counter DegradableCounter -map DegradableMap -ratios 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
+#CLASSPATH=../java/target/*:../java/target/lib/* java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.App -set DegradableSet -queue MapQueue -counter DegradableCounter -map DegradableMap -distribution 5 15 30 50 -nbTest 5 -time 10 -wTime 5 -s -p
