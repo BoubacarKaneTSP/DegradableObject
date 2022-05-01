c2f2893fdc0775dc5a32ef033f505c5e7d44ffbe
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-05-01 17:14:25 +0200

    updating DegradableMap.get operation to allow a thread to read another thread's map

--- java/src/main/java/eu/cloudbutton/dobj/Map/DegradableMap.java
+++ java/src/main/java/eu/cloudbutton/dobj/Map/DegradableMap.java
@@ -1,7 +1,9 @@
 package eu.cloudbutton.dobj.Map;
 
+import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
+import java.rmi.NoSuchObjectException;
 import java.util.AbstractMap;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -31,9 +33,25 @@ public class DegradableMap<K,V> extends AbstractMap<K,V> {
         return local.get().remove(key);
     }
 
+    @SneakyThrows
     @Override
     public V get(Object key) {
-        return local.get().get(key);
+        V value;
+
+        value = local.get().get(key);
+
+        if (value != null)
+            return value;
+
+        for (AbstractMap<K,V> map : map.values()){
+            value = map.get(key);
+
+            if (value != null)
+                return value;
+        }
+
+
+        throw new NullPointerException();
     }
 
     @NotNull
