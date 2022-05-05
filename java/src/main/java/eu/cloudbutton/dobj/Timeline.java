29397a2d790ab13716d659bb906f9b3091e2d654
otrack <0track@gmail.com>
2022-05-05 17:19:43 +0200

    timeline fix

--- java/src/main/java/eu/cloudbutton/dobj/Timeline.java
+++ java/src/main/java/eu/cloudbutton/dobj/Timeline.java
@@ -1,6 +1,7 @@
 package eu.cloudbutton.dobj;
 
 import eu.cloudbutton.dobj.Counter.AbstractCounter;
+import org.apache.log4j.helpers.BoundedFIFO;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -8,41 +9,28 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Timeline<T> {
 
+    private final static int LENGTH=50;
+
+    private final LinkedList<T> topk = new LinkedList<>();
     private final AbstractQueue<T> timeline;
-    private final AbstractCounter size;
     private AtomicBoolean flag = new AtomicBoolean();
 
     public Timeline(AbstractQueue timeline, AbstractCounter size) {
         this.timeline = timeline;
-        this.size = size;
         this.flag.set(true);
     }
 
     public void add(T elt){
-
-        if (flag.get()){
-            size.incrementAndGet();
-            if (size.read() >= 50) {
-                flag.set(false);
-            }
-            timeline.offer(elt);
-        }else{
-            timeline.offer(elt);
-            timeline.poll();
-        }
+        timeline.offer(elt);
    }
 
-   public AbstractQueue<T> read(){
-
-       /*Collection<T> ret = new ArrayList<>();
-
-       Iterator<T> it = timeline.iterator();
-       int i = 0;
-
-       while (it.hasNext() && i < 50) {
-           ret.add(it.next());
-           i++;
-       }*/
-       return timeline;
+   public List<T> read(){
+       T e = timeline.poll();
+       if (e!=null) {
+           topk.add(e);
+           if (topk.size()>50)
+               topk.poll();
+       }
+       return topk;
    }
 }
