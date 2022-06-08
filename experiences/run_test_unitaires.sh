a52791936bb267dbad9b7ec47886f2c8e5591a07
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-06-08 09:28:23 +0000

    results test micro benchmark with longer workloadTime

---
+++ experiences/run_test_unitaires.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+save=""
+print=""
+compile=""
+workloadTime=10
+warmingUpTime=5
+numberOfTest=1
+
+while getopts 'oepxw:u:n:' OPTION; do
+	case "$OPTION" in
+		o)
+			echo "script usage: $(basename \$0)
+			[-o] print option,
+			[-e] save results,
+			[-p] print results,
+			[-x] compile the project
+			[-w] workload time in seconds
+			[-u] warming up time in seconds
+			[-n] number of test" >&2
+			exit 1
+			;;
+		e)
+			save="-e "
+			;;
+		p)
+			print="-p "
+			;;
+		x)
+			compile="-x "
+			;;
+		w)
+			workloadTime="$OPTARG"
+			;;
+		u)
+			warmingUpTime="$OPTARG"
+			;;
+		n)
+			numberOfTest="$OPTARG"
+			;;
+		?)
+			echo "print the option with [-o]" >&2
+			exit 1
+			;;
+	esac
+done
+
+for type in "Set" "DegradableSet" "Map" "DegradableMap" "Queue" "DegradableList" #"Counter" "DegradableCounter"
+do
+	./test.sh -c $type -t Benchmark -k -w $workloadTime -u $warmingUpTime -n $numberOfTest $print$compile$save
+done
