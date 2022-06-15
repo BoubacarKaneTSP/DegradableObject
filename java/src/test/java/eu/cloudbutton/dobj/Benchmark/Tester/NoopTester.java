20d4a3e0cc9c03145671ef3472635052a48679e4
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-06-15 18:28:06 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/Benchmark/Tester/NoopTester.java
+++ java/src/test/java/eu/cloudbutton/dobj/Benchmark/Tester/NoopTester.java
@@ -14,27 +14,27 @@ public class NoopTester extends Tester<Noop> {
     protected long test(opType type) {
         // no-op
 
-        long startTime = 0L, endTime = 0L;
+        long startTime = 1L, endTime = 1L;
 
         int n = random.nextInt(ITEM_PER_THREAD);
         switch (type) {
             case ADD:
                 startTime = System.nanoTime();
-                for (int i = 0; i < 2000; i++) {
+                for (int i = 0; i < 5000; i++) {
                     n++;
                 }
                 endTime = System.nanoTime();
                 break;
             case REMOVE:
                 startTime = System.nanoTime();
-                for (int i = 0; i < 2000; i++) {
+                for (int i = 0; i < 5000; i++) {
                     n--;
                 }
                 endTime = System.nanoTime();
                 break;
             case READ:
                 startTime = System.nanoTime();
-                for (int i = 0; i < 2000; i++) {
+                for (int i = 0; i < 5000; i++) {
                     n += 2;
                 }
                 endTime = System.nanoTime();
@@ -42,6 +42,6 @@ public class NoopTester extends Tester<Noop> {
                 break;
         }
 
-        return (endTime - startTime)/2000;
+        return (endTime - startTime)/5000;
     }
 }
