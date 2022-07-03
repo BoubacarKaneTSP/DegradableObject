0c67c3c78b8cb0b936e33be8397fbb7b5fc9f0f1
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-07-03 20:51:11 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/map/CollisionKey.java
+++ java/src/test/java/eu/cloudbutton/dobj/map/CollisionKey.java
@@ -2,13 +2,14 @@ package eu.cloudbutton.dobj.map;
 
 import lombok.Data;
 import nl.peterbloem.powerlaws.DiscreteApproximate;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Data
-public class CollisionKey /*implements Comparable<CollisionKey>*/{
+public class CollisionKey implements Comparable<CollisionKey>{
 
      String value;
      List<Integer> listHashCode = new ArrayList<>();
@@ -45,9 +46,9 @@ public class CollisionKey /*implements Comparable<CollisionKey>*/{
     public int hashCode() {
         return listHashCode.get(random.nextInt(listHashCode.size()));
     }
-/*
+
     @Override
     public int compareTo(@NotNull CollisionKey o) {
         return this.value.hashCode() - o.hashCode();
-    }*/
+    }
 }
