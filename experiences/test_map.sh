d2ca08987291eb1b845ebf053055166382d8bc63
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-07-07 21:34:47 +0200

    update

--- experiences/test_map.sh
+++ experiences/test_map.sh
@@ -4,7 +4,10 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-for nbInitialAdd in 10 100 1000 10000 100000 1000000 10000000
+for nbThread in 2 40
 do
-CLASSPATH=../java/target/*:../java/target/lib/* numactl -N 1 -m 0 java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.Benchmark -type Map -ratios "100 0 0" -nbThreads 1 -nbTest 5 -time 20 -wTime 10 -nbOps $nbInitialAdd -p -s -asymmetric -collisionKey >> Map_1_thread.txt
+  for nbInitialAdd in 10 100 1000 10000 100000 1000000 10000000
+  do
+    CLASSPATH=../java/target/*:../java/target/lib/* numactl -N 1 -m 0 java -XX:+UseNUMA -XX:+UseG1GC eu.cloudbutton.dobj.Benchmark.Benchmark -type Map -ratios "100 0 0" -nbThreads $nbThread -nbTest 5 -time 20 -wTime 10 -nbOps $nbInitialAdd -p -s -asymmetric -collisionKey >> Map_1_thread.txt
+  done
 done
