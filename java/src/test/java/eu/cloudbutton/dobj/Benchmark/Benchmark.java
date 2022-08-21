5584095fa3926391a13049c7548d3a338e394531
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-08-21 18:28:35 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/Benchmark/Benchmark.java
+++ java/src/test/java/eu/cloudbutton/dobj/Benchmark/Benchmark.java
@@ -140,8 +140,8 @@ public class Benchmark {
                 for (int _nbTest = 0; _nbTest < nbTest; _nbTest++) {
 
 //                     We re-fill the object only if this the first time we use it and we only test READ
-                    if ((nbCurrentThread == 1 || (nbCurrentThread == 2 && _asymmetric)) && Arrays.stream(ratios).mapToInt(Integer::parseInt).toArray()[2] == 95
-                    || Arrays.stream(ratios).mapToInt(Integer::parseInt).toArray()[2] != 95){
+                    if ((nbCurrentThread == 1 || (nbCurrentThread == 2 && _asymmetric)) && Arrays.stream(ratios).mapToInt(Integer::parseInt).toArray()[2] == 100
+                    || Arrays.stream(ratios).mapToInt(Integer::parseInt).toArray()[2] != 100){
                         if (_nbTest == 0)
                             object = Factory.createObject(type);
 
