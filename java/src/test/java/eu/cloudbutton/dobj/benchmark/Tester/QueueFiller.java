54b7d04dc6fcf86ad09e57ab1b056a8bdfcd5c05
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-10-07 11:53:00 +0200

    testing queue perf by changing QueueFiller.java

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Tester/QueueFiller.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Tester/QueueFiller.java
@@ -18,14 +18,16 @@ public class QueueFiller extends Filler<Queue> {
         ExecutorService executor = Executors.newFixedThreadPool(nbThread);
         List<Future<Void>> futures = new ArrayList<>();
 
+        int nbTask = 10;
+
         Callable<Void> callable = () -> {
-            for (int i = 0; i < nbOps/nbThread; i++) {
+            for (int i = 0; i < nbOps/nbTask; i++) {
                 object.add(i);
             }
             return null;
         };
 
-        for (int i = 0; i < nbThread; i++) {
+        for (int i = 0; i < nbTask; i++) {
             futures.add(executor.submit(callable));
         }
 
