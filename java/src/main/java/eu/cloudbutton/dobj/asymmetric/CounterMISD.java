8ac66431d48254c2952c8ea71712590bf9d740db
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-10-10 18:09:08 +0200

    testing boxed long with thread local directly in the class

--- java/src/main/java/eu/cloudbutton/dobj/asymmetric/CounterMISD.java
+++ java/src/main/java/eu/cloudbutton/dobj/asymmetric/CounterMISD.java
@@ -1,17 +1,21 @@
 package eu.cloudbutton.dobj.asymmetric;
 
+import eu.cloudbutton.dobj.incrementonly.BoxedLong;
 import eu.cloudbutton.dobj.incrementonly.Counter;
 import eu.cloudbutton.dobj.incrementonly.CounterIncrementOnly;
 
 public class CounterMISD extends CounterIncrementOnly implements Counter {
 
+    ThreadLocal<BoxedLong> threadLocal;
     public CounterMISD(){
         super();
+        threadLocal = ThreadLocal.withInitial(() -> new BoxedLong());
     }
 
     @Override
     public long decrementAndGet(){
-  //      local.get().val -=1 ;
+        threadLocal.get().val -= 1;
+//        local.get().val -=1 ;
 //        UNSAFE.storeFence();
 
         return 0;
