3dd2077c889cccd8e8cc5f531562b7cde7acd0fa
Boubacar Kane <kaneb@bran>
2022-11-22 00:49:34 +0100

    results

--- experiences/test_map.sh
+++ experiences/test_map.sh
@@ -8,7 +8,7 @@ perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s Concurr
 #./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m MapAddIntensive -t Retwis -p -e -w 15 -u 5 -n 1 -b -h "M"
 #./test.sh -c CounterJUC -s SetAddIntensive -q ConcurrentLinkedQueue -m MapAddIntensive -t Retwis -p -e -w 15 -u 5 -n 1 -b -h "M_S"
 #./test.sh -c CounterJUC -s SetAddIntensive -q QueueMASP -m MapAddIntensive -t Retwis -p -e -w 15 -u 5 -n 1 -b -h "Q_M_S"
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SetAddIntensive -q ConcurrentLinkedQueue -m MapAddIntensive -t Retwis -p -e -w 20 -u 10 -n 5 -i 100000 -b -h "Q_M_S_C"
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m MapAddIntensive -t Retwis -p -e -w 20 -u 10 -n 5 -i 100000 -b -h "Q_M_S_C"
 
 #./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -p -e -w 15 -u 5 -n 5 -z -h "JUC_CompletionTime"
 #./test.sh -c CounterJUC -s ConcurrentSkipListSet -q QueueMASP -m ConcurrentHashMap -t Retwis -p -e -w 15 -u 5 -n 5 -z -h "Q_CompletionTime"
