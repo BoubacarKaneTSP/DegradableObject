fdda83229a7600fda6411fe9358d9b6820680e7a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-11-23 13:51:27 +0100

    update

--- java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
+++ java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
@@ -406,10 +406,6 @@ public class QueueMASP<E> extends AbstractQueue<E>
                 System.exit(0);
             }
             head = head.next;
-            if (head == null) {
-                System.out.println("Passage de head à null par le thread : " + Thread.currentThread().getName());
-                System.exit(0);
-            }
             queueSize.decrementAndGet();
 //            queueSize.decrement();
 //            head.item = null;
