45b76f3fc16b88f08582e58a6e78b9b7222a2dfa
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-12-05 18:48:28 +0100

    Testing and analysing Retwis with 100000 user only tweet and read with original size

--- java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
+++ java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
@@ -243,7 +243,7 @@ public class QueueMASP<E> extends AbstractQueue<E>
      *   to not be reachable from head!
      * - tail.next may or may not be self-linked.
      */
-    @Contended
+//    @Contended
     private transient volatile Node<E> tail;
 
 //    private Counter queueSize;
