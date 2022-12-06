42d139e765762b5b0830eda034a8cb02c6411e33
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-12-06 10:40:02 +0100

    Testing and analysing Retwis with 1000 user

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -9,7 +9,7 @@ do
 	echo $nbThread
 	echo
 
-  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -p -e -w 30 -u 10 -n 3 -i 100 -b -h "JUC" -g $nbThread
-	perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ConcurrentSkipListSet -q QueueMASP -m MapAddIntensive -t Retwis -p -e -w 30 -u 10 -n 3 -i 100 -b -h "Q_M_S_C" -g $nbThread
+  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -p -e -w 30 -u 10 -n 3 -i 1000 -b -h "JUC" -g $nbThread
+	perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ConcurrentSkipListSet -q QueueMASP -m MapAddIntensive -t Retwis -p -e -w 30 -u 10 -n 3 -i 1000 -b -h "Q_M_S_C" -g $nbThread
 
 done
