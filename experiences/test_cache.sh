f2c09b922fa780db54fb8c47ff4ea586bc9cd7b8
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2022-12-30 17:04:18 +0100

    retesting Retwis with large number of users to detect bug

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,14 +4,13 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-initSize=1024
-range=2048
 nbTest=5
 benchmarkTime=30
 warmingUpTime=10
+initNbUsers=1000000
 
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "JUC" -i 100 -k
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SetAddIntensive -q QueueMASP -m MapAddIntensive -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "Q_M_S_C" -i 100 -k
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "JUC" -i $initNbUsers -k
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SetAddIntensive -q QueueMASP -m MapAddIntensive -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "Q_M_S_C" -i -i $initNbUsers -k
 
 ##for nbThread in 1 2 4 8 16 32 48
 #for nbUser in 100 1000 10000 100000 1000000
