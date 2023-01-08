b1349743b45ae77bc02acd262d7b7e8c09029bff
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-01-08 21:33:12 +0100

    update

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -9,7 +9,7 @@ benchmarkTime=20
 warmingUpTime=10
 initNbUsers=1000000
 
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "JUC" -i $initNbUsers -k -j
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "JUC" -i $initNbUsers -k
 #perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SetAddIntensive -q QueueMASP -m MapAddIntensive -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "Q_M_S_C" -i -i $initNbUsers -k
 
 ##for nbThread in 1 2 4 8 16 32 48
