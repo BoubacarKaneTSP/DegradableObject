c7418dfc3e5f81d703ad4eacebb524f6d260ddfd
Boubacar Kane <kaneb@bran>
2023-01-15 11:49:08 +0100

    results

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -10,5 +10,5 @@ nbTest=5
 benchmarkTime=20
 warmingUpTime=10
 
-perf stat -B -e cache-references,cache-misses ./test.sh -s AtomicReference -t Microbenchmark -p -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -j
-perf stat -B -e cache-references,cache-misses ./test.sh -s AtomicWriteOnceReference -t Microbenchmark -p -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -j
+perf stat -B -e cache-references,cache-misses ./test.sh -s AtomicReference -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -j
+perf stat -B -e cache-references,cache-misses ./test.sh -s AtomicWriteOnceReference -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -j
