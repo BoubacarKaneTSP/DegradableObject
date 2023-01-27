0b94b54d953a36d9a59b773176f5120a20fa33db
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-01-27 20:57:11 +0100

    update

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -10,6 +10,6 @@ warmingUpTime=5
 initNbUsers=48
 nbHashCode=48
 
-#perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -j -b
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m SegmentedHashMap -t Retwis -r "5 0 45 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -j -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "5 0 45 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -j -b
+#perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m SegmentedHashMap -t Retwis -r "5 0 45 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -j -b
 
