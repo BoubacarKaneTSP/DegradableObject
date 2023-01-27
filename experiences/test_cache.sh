c4eec890f4b23d94fbffa76d3e7be72d4d8103d2
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-01-27 17:30:19 +0100

    update

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -10,6 +10,6 @@ warmingUpTime=10
 initNbUsers=100
 nbHashCode=100
 
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -s ConcurrentSkipListSet -q ConcurrentLinkedQueue -m ConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -j
+perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -j
 #perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m SegmentedHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -j
 
