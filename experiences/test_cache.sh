b6f81fb21ccf3c1ec7a16ddf9f641b157863646e
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-01-27 18:28:35 +0100

    update

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,11 +4,11 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=5
+nbTest=1
 benchmarkTime=20
 warmingUpTime=10
 initNbUsers=100
-nbHashCode=100
+nbHashCode=1000
 
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -j
 #perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m SegmentedHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -b -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -j
