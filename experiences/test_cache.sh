6294b68e06c8152173b0cac056e1efba0dcaa797
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-11 00:45:35 +0100

    testing retwis with 1000000 users (alpha 1.39)

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,11 +4,11 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
+nbTest=5
 benchmarkTime=20
 warmingUpTime=5
-initNbUsers=100
-nbHashCode=1000
+initNbUsers=1000000
+nbHashCode=10000000
 
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -b
 perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m ShardedHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -b
