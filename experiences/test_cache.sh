6a30e172979a4bcd2dc76858c91aef36e1d4ede6
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-19 13:46:39 +0100

    Retwis 100 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,12 +4,12 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
+nbTest=15
 benchmarkTime=20
 warmingUpTime=5
 initNbUsers=100
 nbHashCode=1000
 
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -i $initNbUsers -d $nbHashCode -b
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedHashSet -q QueueMASP -m ShardedHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ShardedHashSet -q QueueMASP -m ShardedHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -i $initNbUsers -d $nbHashCode -b
 
