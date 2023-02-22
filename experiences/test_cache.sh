803a2012f746abcfde7f50e73228668224574070
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-22 14:04:47 +0100

    testing completion time for 100M operation with 100 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -8,8 +8,8 @@ nbTest=10
 benchmarkTime=20
 warmingUpTime=5
 nbUsersInit=100
-nbHashCode=10000
-nbOps=10000000
+nbHashCode=1000
+nbOps=100000000
 
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b -z
 perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ShardedHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b -z
