718863db0076de7564a4c20fa0b3a1d5105ffe55
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-22 01:53:23 +0100

    testing completion time for 1M operation with 100 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -7,9 +7,9 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 nbTest=10
 benchmarkTime=20
 warmingUpTime=5
-nbUsersInit=10000
-nbHashCode=100000
-nbOps=10000000
+nbUsersInit=100
+nbHashCode=1000
+nbOps=1000000
 
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b -z
 perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ShardedHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "5 15 30 50" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b -z
