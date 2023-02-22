c12394d81305dd42acafd31438af9f7c05a6894d
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-22 14:22:58 +0100

    testing completion time for 100M operation with 100 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
+nbTest=3
 benchmarkTime=20
 warmingUpTime=5
 nbUsersInit=100
