1ab9b59a7cd936c4f06306bd1089663813cb1fd8
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-26 20:04:51 +0100

    Testing Retwis < 1000 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -5,14 +5,14 @@ trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 nbTest=1
-benchmarkTime=30
+benchmarkTime=20
 warmingUpTime=5
 #nbUsersInit=1000
 nbHashCode=1000000
 nbOps=10000000
 ratio="5 15 30 50"
 
-for nbUsersInit in 1000
+for nbUsersInit in 100 400 700 1000
 do
 perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
 perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
