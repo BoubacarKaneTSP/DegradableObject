25dbd7c432e66bb61607852ccc22707c9d44a841
Boubacar Kane <kaneb@bran>
2023-02-28 21:25:53 +0100

    results

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -14,7 +14,7 @@ ratio="5 15 30 50"
 
 for nbUsersInit in 100 10000 1000000
 do
-perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentSkipListSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
 perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
 done
 
