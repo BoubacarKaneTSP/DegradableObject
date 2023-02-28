3acf2679d22ef5f87328de5b26e83dc543b9dc16
Boubacar Kane <kaneb@bran>
2023-02-28 23:20:50 +0100

    results

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -14,8 +14,8 @@ ratio="5 15 30 50"
 
 for nbUsersInit in 100 10000 1000000
 do
-perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s Set -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
 done
 
 
