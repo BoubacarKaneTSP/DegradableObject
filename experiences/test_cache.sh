824fa42df84ee714bc3ee9bdf7fadb55545d78e7
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-02-28 20:22:13 +0100

    retwis alpha 1.39 all nb users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,18 +4,18 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
+nbTest=2
 benchmarkTime=20
 warmingUpTime=5
 #nbUsersInit=1000
 nbHashCode=1000000
 nbOps=100000
-ratio="5 15 30 50"
+ratio="0 15 30 55"
 
 for nbUsersInit in 100 10000 1000000
 do
-perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
-perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c Counter -s ConcurrentSkipListSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "JUC" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
+perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -s SegmentedSkipListSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -n $nbTest -h "Q_M_S_C" -y $nbUsersInit -d $nbHashCode -i $nbOps -b
 done
 
 
