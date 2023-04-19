8bd0935c8723e0dd9bd9b4d1007f493a68c01087
Boubacar Kane <kaneb@bran>
2023-04-19 20:51:04 +0200

    results

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,11 +4,11 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=2
-benchmarkTime=30
-warmingUpTime=6
+nbTest=10
+benchmarkTime=300
+warmingUpTime=60
 #nbUsersInit=1000
-nbHashCode=1000000
+nbHashCode=10000000
 nbOps=100000
 ratio="5 15 30 50"
 
@@ -24,6 +24,9 @@ do
   do
     for (( c=1; c<=nbTest; c++ ))
     do
+	    echo " "
+	    echo $nbThread
+	    echo " "
       perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -j -g $nbThread
       python3 analyse_perf.py perf.log "false" "JUC" $nbThread $nbUsersInit
       perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -j -g $nbThread
