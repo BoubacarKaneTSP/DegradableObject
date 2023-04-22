2934462d1641d21ebe90f9bc652e976c0c6feb40
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-04-23 01:04:14 +0200

    Test retwis

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -158,11 +158,15 @@ public class Database {
 
     public void addUser(Key user) throws ClassNotFoundException {
         mapFollowers.put(user, new ConcurrentSkipListSet<>());
-        if (typeSet.contains("Extended"))
+        mapFollowing.put(user, new ConcurrentSkipListSet<>());
+
+        /*if (typeSet.contains("Extended"))
             mapFollowing.put(user, Factory.createSet(typeSet, factoryIndice));
         else
-            mapFollowing.put(user, Factory.createSet(typeSet, nbThread));
-        mapTimelines.put(user, new Timeline(Factory.createQueue(typeQueue)));
+            mapFollowing.put(user, Factory.createSet(typeSet, nbThread));*/
+
+        mapTimelines.put(user, new Timeline(new ConcurrentLinkedQueue()));
+//        mapTimelines.put(user, new Timeline(Factory.createQueue(typeQueue)));
     }
 
     // Adding user_A to the followers of user_B
