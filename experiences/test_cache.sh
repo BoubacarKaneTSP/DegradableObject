4653b9c1d7961814f4ae8f3a95d880a87c422578
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-04-24 18:48:30 +0200

    Test retwis 3min long

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,8 +4,8 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=10
-benchmarkTime=120
+nbTest=5
+benchmarkTime=180
 warmingUpTime=10
 #nbUsersInit=1000
 nbHashCode=10000000
@@ -20,6 +20,7 @@ do
   for op in "ALL" "ADD" "FOLLOW" "UNFOLLOW" "TWEET" "READ"
   do
     rm "${op}_JUC_139_${nbUsersInit}.txt"
+    rm "${op}_Q_M_C_139_${nbUsersInit}.txt"
     rm "${op}_Q_M_S_C_139_${nbUsersInit}.txt"
   done
 
@@ -30,16 +31,19 @@ do
 	    echo " "
 	    echo $nbThread
 	    echo " "
-      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
-#      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -j -g $nbThread
-      python3 analyse_perf.py perf.log "false" "Q_M_S_C" $nbThread $nbUsersInit
-      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
+      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
       python3 analyse_perf.py perf.log "false" "JUC" $nbThread $nbUsersInit
+      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
+      python3 analyse_perf.py perf.log "false" "Q_M_C" $nbThread $nbUsersInit
+      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ExtendedConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
+      python3 analyse_perf.py perf.log "false" "Q_M_S_C" $nbThread $nbUsersInit
     done
   done
   python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 2 4 8 16 32 48"
+  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 2 4 8 16 32 48"
   python3 compute_avg_throughput.py $nbUsersInit "Q_M_S_C" "1 2 4 8 16 32 48"
   python3 analyse_perf.py perf.log "true" "JUC" $nbThread $nbUsersInit
+  python3 analyse_perf.py perf.log "true" "Q_M_C" $nbThread $nbUsersInit
   python3 analyse_perf.py perf.log "true" "Q_M_S_C" $nbThread $nbUsersInit
 
 done
