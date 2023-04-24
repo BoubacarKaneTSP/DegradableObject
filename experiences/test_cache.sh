ca0298a2396054919337364b7ae5252405bbcf66
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-04-24 02:07:48 +0200

    Test retwis 2min long

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,15 +4,17 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=20
-benchmarkTime=60
+nbTest=10
+benchmarkTime=120
 warmingUpTime=10
 #nbUsersInit=1000
 nbHashCode=10000000
 nbOps=100000
 ratio="5 15 30 50"
 
-for nbUsersInit in 100 #10000 1000000
+#ExtendedSegmentedConcurrentHash
+
+for nbUsersInit in 100 10000 1000000
 do
   # Cleaning old file
   for op in "ALL" "ADD" "FOLLOW" "UNFOLLOW" "TWEET" "READ"
@@ -31,7 +33,7 @@ do
       perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
 #      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_S_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -j -g $nbThread
       python3 analyse_perf.py perf.log "false" "Q_M_S_C" $nbThread $nbUsersInit
-      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
+      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
       python3 analyse_perf.py perf.log "false" "JUC" $nbThread $nbUsersInit
     done
   done
