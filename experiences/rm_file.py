5b71f998667dd79bba44df83dd693d122b122b70
Boubacar Kane <kaneb@bran>
2023-05-14 12:56:05 +0200

    results

--- experiences/rm_file.py
+++ experiences/rm_file.py
@@ -7,4 +7,4 @@ type_obj = sys.argv[2]
 list_op = ["ALL", "ADD", "FOLLOW", "UNFOLLOW", "TWEET", "READ", "avg_queue_size", "avg_Follower", "proportion_Max_Follower", "proportion_User_With_Max_Follower", "proportion_User_Without_Follower", "nb_user_final", "nb_tweet_final"]
 
 for op in list_op:
-    os.remove(op+"_"+type_obj+"_139_"+nb_user+".txt")
+    os.remove(op+"_"+type_obj+"_"+nb_user+".txt")
