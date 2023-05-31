d2e549c876c95a7bf54a0f6efdac5166b3bbce81
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-05-31 09:24:12 +0200

    Testing retwis 100K users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -5,8 +5,8 @@ trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 nbTest=1
-benchmarkTime=900
-warmingUpTime=120
+benchmarkTime=300
+warmingUpTime=60
 #nbUsersInit=1000
 nbHashCode=10000000
 nbOps=100000
@@ -14,14 +14,14 @@ ratio="0 10 40 50"
 
 #ExtendedSegmentedConcurrentHash
 
-for nbUsersInit in 100 10000 1000000
+for nbUsersInit in 100 100000
 do
   # Cleaning old file
   python3 rm_file.py $nbUsersInit "JUC"
   python3 rm_file.py $nbUsersInit "Q_M_C"
 #  python3 rm_file.py $nbUsersInit "Q_M_S_C"
 
-  for nbThread in 1 2 4 8 16 32 48
+  for nbThread in 1 8 48
   do
     for (( c=1; c<=nbTest; c++ ))
     do
@@ -38,8 +38,8 @@ do
       #python3 analyse_perf.py perf.log "false" "Q_M_S_C" $nbThread $nbUsersInit
     done
   done
-  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 2 4 8 16 32 48"
-  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 2 4 8 16 32 48"
+  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 8 48"
+  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 8 48"
   #python3 compute_avg_throughput.py $nbUsersInit "Q_M_S_C" "1 2 4 8 16 32 48"
   python3 analyse_perf.py perf.log "true" "JUC" $nbThread $nbUsersInit
   python3 analyse_perf.py perf.log "true" "Q_M_C" $nbThread $nbUsersInit
