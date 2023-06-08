9cea86761dfb5ab5fc880319d323833604af9d28
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-06-08 18:57:29 +0200

    testing btrace

--- experiences/ScanBtrace.java
+++ experiences/ScanBtrace.java
@@ -1,60 +1,75 @@
-import com.sun.btrace.annotations.*;
-import static com.sun.btrace.BTraceUtils.*;
+import org.openjdk.btrace.core.annotations.BTrace;
+import org.openjdk.btrace.core.annotations.Export;
+import org.openjdk.btrace.core.annotations.Location;
+import org.openjdk.btrace.core.annotations.OnMethod;
+import org.openjdk.btrace.core.annotations.OnTimer;
+import org.openjdk.btrace.core.annotations.ProbeClassName;
+import org.openjdk.btrace.core.annotations.ProbeMethodName;
+import org.openjdk.btrace.core.annotations.Property;
+import org.openjdk.btrace.core.annotations.Self;
+import org.openjdk.btrace.core.annotations.Duration;
+import org.openjdk.btrace.core.annotations.Kind;
+import org.openjdk.btrace.core.BTraceUtils.Aggregations;
+import org.openjdk.btrace.core.aggregation.Aggregation;
+import org.openjdk.btrace.core.aggregation.AggregationFunction;
+import static org.openjdk.btrace.core.BTraceUtils.*;
 
 @BTrace
 public class ScanBtrace {
 
-    // Chronomètres pour chaque méthode
-    private static volatile BTraceTimer addTimer = BTraceUtils.timer("add");
-    private static volatile BTraceTimer followTimer = BTraceUtils.timer("follow");
-    private static volatile BTraceTimer unfollowTimer = BTraceUtils.timer("unfollow");
-    private static volatile BTraceTimer tweetTimer = BTraceUtils.timer("tweet");
-    private static volatile BTraceTimer readTimer = BTraceUtils.timer("read");
+    private static Aggregation addMethodDuration = Aggregations.newAggregation(AggregationFunction.AVERAGE);
+    private static Aggregation followMethodDuration = Aggregations.newAggregation(AggregationFunction.AVERAGE);
+    private static Aggregation unfollowMethodDuration = Aggregations.newAggregation(AggregationFunction.AVERAGE);
+    private static Aggregation tweetMethodDuration = Aggregations.newAggregation(AggregationFunction.AVERAGE);
+    private static Aggregation readMethodDuration = Aggregations.newAggregation(AggregationFunction.AVERAGE);
 
-    // Méthode appelée au début de l'exécution
-    @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Retwis", method = "startMonitoring")
-    public static void onStartMonitoring() {
-        System.out.println("D2BUT DUUUUU MMOOONITOOORING");
-        addTimer.reset();
-        followTimer.reset();
-        unfollowTimer.reset();
-        tweetTimer.reset();
-        readTimer.reset();
-    }
+    @Export
+    private static long addMethodCalls;
+    @Export
+    private static long followMethodCalls;
+    @Export
+    private static long unfollowMethodCalls;
+    @Export
+    private static long tweetMethodCalls;
+    @Export
+    private static long readMethodCalls;
 
-    // Méthode appelée après l'exécution de chaque méthode
     @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Database", method = "add", location = @Location(Kind.RETURN))
-    public static void onAdd() {
-        addTimer.pulse();
+    public static void onAddMethod(@Duration long duration) {
+        Aggregations.addToAggregation(addMethodDuration, duration / 1000000);
+        addMethodCalls++;
     }
 
     @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Database", method = "follow", location = @Location(Kind.RETURN))
-    public static void onFollow() {
-        followTimer.pulse();
+    public static void onFollowMethod(@Duration long duration) {
+        Aggregations.addToAggregation(followMethodDuration, duration / 1000000);
+        followMethodCalls++;
     }
 
     @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Database", method = "unfollow", location = @Location(Kind.RETURN))
-    public static void onUnfollow() {
-        unfollowTimer.pulse();
+    public static void onUnfollowMethod(@Duration long duration) {
+        Aggregations.addToAggregation(unfollowMethodDuration, duration / 1000000);
+        unfollowMethodCalls++;
     }
 
     @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Database", method = "tweet", location = @Location(Kind.RETURN))
-    public static void onTweet() {
-        tweetTimer.pulse();
+    public static void onTweetMethod(@Duration long duration) {
+        Aggregations.addToAggregation(tweetMethodDuration, duration / 1000000);
+        tweetMethodCalls++;
     }
 
     @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Database", method = "read", location = @Location(Kind.RETURN))
-    public static void onRead() {
-        readTimer.pulse();
+    public static void onReadMethod(@Duration long duration) {
+        Aggregations.addToAggregation(readMethodDuration, duration / 1000000);
+        readMethodCalls++;
     }
 
-    // Méthode appelée à la fin de l'exécution
-    @OnMethod(clazz = "eu.cloudbutton.dobj.benchmark.Retwis", method = "stopMonitoring")
-    public static void onStopMonitoring() {
-        println("Débit de la méthode add : " + addTimer.rate() + " appels/ms");
-        println("Débit de la méthode follow : " + followTimer.rate() + " appels/ms");
-        println("Débit de la méthode unfollow : " + unfollowTimer.rate() + " appels/ms");
-        println("Débit de la méthode tweet : " + tweetTimer.rate() + " appels/ms");
-        println("Débit de la méthode read : " + readTimer.rate() + " appels/ms");
+    @OnTimer(10000)
+    public static void printMethodThroughput() {
+        println("Débit de la méthode add : " + (addMethodCalls / 10) + " appels/s");
+        println("Débit de la méthode follow : " + (followMethodCalls / 10) + " appels/s");
+        println("Débit de la méthode unfollow : " + (unfollowMethodCalls / 10) + " appels/s");
+        println("Débit de la méthode tweet : " + (tweetMethodCalls / 10) + " appels/s");
+        println("Débit de la méthode read : " + (readMethodCalls / 10) + " appels/s");
     }
 }
