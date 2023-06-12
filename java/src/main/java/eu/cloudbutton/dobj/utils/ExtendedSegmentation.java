7d95692e9559a84a3da8355c702b3b884dc877ec
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-06-12 17:37:46 +0200

    degrading Queue + Set

--- java/src/main/java/eu/cloudbutton/dobj/utils/ExtendedSegmentation.java
+++ java/src/main/java/eu/cloudbutton/dobj/utils/ExtendedSegmentation.java
@@ -41,22 +41,22 @@ public class ExtendedSegmentation<T> implements Segmentation<T>{
             indice = factoryIndice.getIndice();
 
             if (!obj.getReference().set(indice)){
-//                System.out.println(Thread.currentThread().getName() + " failed to initialize indice "+ indice + " for obj : " + x);
+                System.out.println(Thread.currentThread().getName() + " failed to initialize indice "+ indice + " for obj : " + x);
                 indice = obj.getReference().get();
-//                System.out.println(Thread.currentThread().getName() + " have now indice : " + indice + " for obj : " + x);
+                System.out.println(Thread.currentThread().getName() + " have now indice : " + indice + " for obj : " + x);
 
-//                System.out.println(Thread.currentThread().getName() + " is verifiying if the indice it gets have not change");
+                System.out.println(Thread.currentThread().getName() + " is verifiying if the indice it gets have not change");
 
                 assert indice == obj.getReference().get() : indice + " : " + obj.getReference().get();
             }else{
-//                System.out.println(Thread.currentThread().getName() + " successfully initialized indice  for obj : " + x);
+                System.out.println(Thread.currentThread().getName() + " successfully initialized indice  for obj : " + x);
                 assert System.identityHashCode(indice) == System.identityHashCode(obj.getReference().get()) : "failed to insert in atomic reference";
             }
         }
 
         assert indice != null : "Indice is null for " + Thread.currentThread().getName();
 
-//        System.out.println(Thread.currentThread().getName() + " => " + System.identityHashCode(indice) + " : " + indice);
+        System.out.println(Thread.currentThread().getName() + " => " + System.identityHashCode(indice) + " : " + indice);
 
         segment = segments().get((int) indice.getVal());
 
@@ -64,7 +64,7 @@ public class ExtendedSegmentation<T> implements Segmentation<T>{
 
         assert segment != null : "Value not associated with a segment";
 
-        System.out.println("Segment for " + x + " : " + segment);
+        System.out.println("Segment for " + x + " : " + segment.hashCode());
         return segment;
     }
 
