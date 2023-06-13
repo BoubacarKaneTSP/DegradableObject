b38a860d81108801d3baebe0c3a148da36d112c3
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-06-13 10:13:50 +0200

    degrading all without offer in add

--- java/src/main/java/eu/cloudbutton/dobj/Timeline.java
+++ java/src/main/java/eu/cloudbutton/dobj/Timeline.java
@@ -18,7 +18,7 @@ public class Timeline<T> {
     }
 
     public void add(T elt) throws InterruptedException {
-        timeline.offer(elt);
+//        timeline.offer(elt);
    }
 
    public void realadd(T elt) throws InterruptedException {
