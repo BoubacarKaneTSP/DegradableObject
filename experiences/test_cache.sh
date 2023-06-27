032d034fe76ecc37b5e1c150fa9516004cfed19d
Boubacar Kane <kaneb@bran>
2023-06-27 13:59:39 +0200

    results l1d pending miss 1000 users

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -5,8 +5,8 @@ trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 nbTest=3
-benchmarkTime=10
-warmingUpTime=5
+benchmarkTime=30
+warmingUpTime=30
 #nbUsersInit=1000
 nbHashCode=10000000
 nbOps=100000
@@ -14,7 +14,7 @@ ratio="5 15 30 50"
 
 #ExtendedSegmentedConcurrentHash
 
-for nbUsersInit in 100
+for nbUsersInit in 1000
 do
   # Cleaning old file
   python3 rm_file.py $nbUsersInit "JUC"
