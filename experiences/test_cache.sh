9db5534dbd09a056c15e9fb4ced87716cf240496
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-08-03 21:30:55 +0200

    test retwis JUC vs DEG vs SEQ

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=1
+nbTest=5
 benchmarkTime=30
 warmingUpTime=30
 #nbUsersInit=1000
@@ -14,7 +14,7 @@ ratio="5 15 30 50"
 
 #ExtendedSegmentedConcurrentHash
 
-for nbUsersInit in 100 10000 1000000
+for nbUsersInit in 100 #10000 1000000
 do
   #  Cleaning old file
   python3 rm_file.py $nbUsersInit "JUC"
