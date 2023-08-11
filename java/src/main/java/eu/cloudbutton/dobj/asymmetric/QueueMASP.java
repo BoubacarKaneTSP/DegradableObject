9acd64c7eed6170e071d705fda12940293bd153a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-08-11 15:09:41 +0200

    test retwis 1M users dap simple key

--- java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
+++ java/src/main/java/eu/cloudbutton/dobj/asymmetric/QueueMASP.java
@@ -374,6 +374,33 @@ public class QueueMASP<E> extends AbstractQueue<E>
                     // and for newNode to become "live".
                     if (p != t) // hop two nodes at a time; failure is OK
                         TAIL.weakCompareAndSet(this, t, newNode);
+                    return true;
+                }
+                // Lost CAS race to another thread; re-read next
+            }
+            else if (p == q)
+                // We have fallen off list.  If tail is unchanged, it
+                // will also be off-list, in which case we need to
+                // jump to head, from which all live nodes are always
+                // reachable.  Else the new tail is a better bet.
+                p = (t != (t = tail)) ? t : head;
+            else
+                // Check for tail updates after two hops.
+                p = (p != t && t != (t = tail)) ? t : q;
+        }
+
+        /*final Node<E> newNode = new Node<E>(Objects.requireNonNull(e));
+
+        for (Node<E> t = tail, p = t;;) {
+            Node<E> q = p.next;
+            if (q == null) {
+                // p is last node
+                if (NEXT.compareAndSet(p, null, newNode)) {
+                    // Successful CAS is the linearization point
+                    // for e to become an element of this queue,
+                    // and for newNode to become "live".
+                    if (p != t) // hop two nodes at a time; failure is OK
+                        TAIL.weakCompareAndSet(this, t, newNode);
 
 //                    queueSize.incrementAndGet();
                     queueSize.increment();
@@ -391,7 +418,7 @@ public class QueueMASP<E> extends AbstractQueue<E>
             else
                 // Check for tail updates after two hops.
                 p = (p != t && t != (t = tail)) ? t : q;
-        }
+        }*/
     }
 
     public E poll() {
