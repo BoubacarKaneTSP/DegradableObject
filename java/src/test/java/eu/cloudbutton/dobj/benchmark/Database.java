b79091efb5b400b709922e0a2003a92b62c764cb
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-08-15 19:14:56 +0200

    test retwis with user distribution that follow a powerlaw

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -379,11 +379,11 @@ public class Database {
     public void tweet(Key user, String msg) throws InterruptedException {
         Set<Key> set = mapFollowers.get(user);
 
-        for (Key follower : set) {
+//        for (Key follower : set) {
 //            Timeline timeline = mapTimelines.get(follower);
 //
 //            timeline.add(msg);
-        }
+//        }
     }
 
     public void showTimeline(Key user) throws InterruptedException {
