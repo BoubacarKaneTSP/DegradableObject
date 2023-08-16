7823fb976b667662347d6152ef7b0ebf09cf4e61
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-08-16 03:57:06 +0200

    test retwis 1M users simple key but follow any other at the beginning (usr usage distribution)

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -5,8 +5,8 @@ trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 nbTest=3
-benchmarkTime=30
-warmingUpTime=30
+benchmarkTime=5
+warmingUpTime=10
 #nbUsersInit=1000
 nbHashCode=10000000
 nbOps=50000000000
@@ -24,8 +24,8 @@ do
 #  python3 rm_file.py $nbUsersInit "Q_M_S_C"
 
 #  for nbThread in 1 2 4 8 16 32 48
-#  for nbThread in 1 16 48
-  for nbThread in 48
+  for nbThread in 1 16 48
+#  for nbThread in 48
 #  for nbThread in 1 4 8
 #  for nbThread in 1
   do
@@ -49,14 +49,14 @@ do
     done
   done
 #  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 2 4 8 16 32 48" $completion_time
-#  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 16 48" $completion_time
-  python3 compute_avg_throughput.py $nbUsersInit "JUC" "48" $completion_time
+  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 16 48" $completion_time
+#  python3 compute_avg_throughput.py $nbUsersInit "JUC" "48" $completion_time
 #  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1 4 8" $completion_time
 #  python3 compute_avg_throughput.py $nbUsersInit "JUC" "1" $completion_time
 
 #  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 2 4 8 16 32 48" $completion_time
-# python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 16 48" $completion_time
- python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "48" $completion_time
+ python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 16 48" $completion_time
+# python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "48" $completion_time
 # python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 4 8" $completion_time
 #  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1" $completion_time
 
