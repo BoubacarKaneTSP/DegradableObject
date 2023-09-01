4ec4c22e9bb3b70b471ece7cd1b25313b4387591
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-09-01 19:59:43 +0200

    computing Retwis with RetwisKey and powerlaw distribution for usage and following

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -456,13 +456,13 @@ public class Database {
         Set<Key> set = mapFollowers.get(user);
 
 //        System.out.println(set.size());
-//        int i = 0;
+        int i = 0;
         for (Key follower : set) {
             Timeline timeline = mapTimelines.get(follower);
 //
             timeline.add(msg);
-//            if (++i >= 1000)
-//                break;
+            if (++i >= 1000)
+                break;
         }
     }
 
