bae7d68d7e580cd4e195577d14541e5dbb442a1c
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-09-18 22:55:20 +0200

    Test simple key usage shape 1

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -68,7 +68,7 @@ do
 
 #  python3 compute_avg_throughput.py $nbUsersInit "Q_M_S_C" "1 2 4 8 16 32 48"
 
-  python3 compute_avg_gc_usage.py $nbUsersInit "JUC" "1 2 4 8 16 32 48" $completion_time
+  python3 compute_avg_gc.py $nbUsersInit "JUC" "1 2 4 8 16 32 48" $completion_time
 #  python3 compute_avg_gc_usage.py $nbUsersInit "JUC" "1 4 8" $completion_time
 
   python3 analyse_perf.py perf.log "true" "JUC" $nbThread $nbUsersInit
