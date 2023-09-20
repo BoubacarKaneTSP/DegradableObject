7d6a5166924706c0eea6a7feab6d2aff6fb7baf1
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-09-20 22:30:25 +0200

    Test simple key usage shape 100

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -86,7 +86,7 @@ public class Database {
         mapNbFollowers = new ConcurrentHashMap<>();
         threadID = new ThreadLocal<>();
 
-        List<Integer> powerLawArray = generateValues(nbUsers, nbUserMax, 200, SCALEUSAGE);
+        List<Integer> powerLawArray = generateValues(nbUsers, nbUserMax, 100, SCALEUSAGE);
 
 //        Collections.sort(powerLawArray);
 
