4949089151a45cafc0dff4ce8f30bad5131987d3
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-09-26 20:50:58 +0200

    Test simple key usage shape 600 with deg no set

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -83,7 +83,7 @@ public class Database {
         mapNbFollowers = new ConcurrentHashMap<>();
         threadID = new ThreadLocal<>();
 
-        List<Integer> powerLawArray = generateValues(nbUsers, nbUserMax, 1, SCALEUSAGE);
+        List<Integer> powerLawArray = generateValues(nbUsers, nbUserMax, 600, SCALEUSAGE);
 
 //        Collections.sort(powerLawArray);
 
@@ -526,7 +526,3 @@ public class Database {
     }
 
 }
-
-
-
-
