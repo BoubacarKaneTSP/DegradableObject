4624e93774ed7a3b3610ea18dac982dfe1588d3a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-11-08 14:08:37 +0100

    testing retwis with a complete graph and 1 user per thread

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -4,9 +4,9 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=30
-benchmarkTime=60
-warmingUpTime=20
+nbTest=2
+benchmarkTime=10
+warmingUpTime=5
 #nbUsersInit=1000
 nbHashCode=10000000
 nbOps=50000000000
@@ -18,7 +18,7 @@ for nbUsersInit in 100000
 do
   #  Cleaning old file
   python3 rm_file.py $nbUsersInit "JUC" $completion_time
-  python3 rm_file.py $nbUsersInit "Q_M_C" $completion_time
+#  python3 rm_file.py $nbUsersInit "Q_M_C" $completion_time
 #  python3 rm_file.py $nbUsersInit "SEQ" $completion_time
 #  rm JUC_${nbUsersInit}_gc_usage.txt
 #  rm Q_M_C_${nbUsersInit}_gc_usage.txt
@@ -41,9 +41,9 @@ do
 #      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh -c Counter -s HashSet -q SequentialQueue -m ShardedHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "SEQ" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread #-z $nbOps
 #      python3 analyse_perf.py perf.log "false" "SEQ" $nbThread $nbUsersInit
 
-      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread #-j #-z $nbOps
+#      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread #-j #-z $nbOps
+#      python3 analyse_perf.py perf.log "false" "Q_M_C" $nbThread $nbUsersInit
 #      perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh -c Counter -s ExtendedSegmentedHashSet -q QueueMASP -m ExtendedSegmentedConcurrentHashMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread
-      python3 analyse_perf.py perf.log "false" "Q_M_C" $nbThread $nbUsersInit
 
       perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh -c CounterJUC -s ConcurrentHashSet -q Queue -m Map -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread #-j #-z $nbOps
       python3 analyse_perf.py perf.log "false" "JUC" $nbThread $nbUsersInit
@@ -61,7 +61,7 @@ do
 
 #  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 2 4 8 16 32 48" $completion_time
 # python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "2 16 48" $completion_time
- python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 32 96" $completion_time
+# python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 32 96" $completion_time
 # python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "48" $completion_time
 # python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1 4 8" $completion_time
 #  python3 compute_avg_throughput.py $nbUsersInit "Q_M_C" "1" $completion_time
@@ -79,7 +79,7 @@ do
 #  python3 compute_avg_gc_usage.py $nbUsersInit "Q_M_C" "1 4 8" $completion_time
 
   python3 analyse_perf.py perf.log "true" "JUC" $nbThread $nbUsersInit
-  python3 analyse_perf.py perf.log "true" "Q_M_C" $nbThread $nbUsersInit
+#  python3 analyse_perf.py perf.log "true" "Q_M_C" $nbThread $nbUsersInit
 #  python3 analyse_perf.py perf.log "true" "SEQ" $nbThread $nbUsersInit
   #python3 analyse_perf.py perf.log "true" "Q_M_S_C" $nbThread $nbUsersInit
 
