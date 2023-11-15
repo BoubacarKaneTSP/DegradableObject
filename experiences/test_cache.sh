a517c6b528e0a5ac880f293d92a19527335beb40
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-11-15 16:31:47 +0100

    test

--- experiences/test_cache.sh
+++ experiences/test_cache.sh
@@ -11,7 +11,7 @@ warmingUpTime=3
 nbHashCode=10000000
 nbOps=50000000000
 #ratio="0 0 0 0 100 0"
-ratio="5 10 20 35 15 15"
+ratio="0 10 25 35 15 15"
 completion_time="False"
 #ExtendedSegmentedConcurrentHash
 
