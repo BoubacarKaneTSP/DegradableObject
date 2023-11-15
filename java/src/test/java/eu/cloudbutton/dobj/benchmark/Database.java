e81411e2b27923b2434e47093666488b3317aa45
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-11-15 15:30:58 +0100

    test

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -588,7 +588,7 @@ public class Database {
         }
 
         for (int i = 0; i < nbThread; i++) {
-            for (int j = 0; j < nbThread; j++) {
+            for (int j = 0; j < 10; j++) {
 
                 if (i != j+i*10){
                     System.out.println("follow");
