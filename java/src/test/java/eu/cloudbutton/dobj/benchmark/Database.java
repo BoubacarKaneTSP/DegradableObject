c5047d9057e8ae07d026d6a10466ce37da2ed555
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-11-27 11:52:37 +0100

    test ThreadLocalRandom

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -815,7 +815,6 @@ public class Database {
     // user_A  is following user_B
     public void followUser(Key userA, Key userB) throws InterruptedException {
 
-        System.out.println();
         mapFollowers.get(userB)
                 .add(userA);
         mapFollowing.get(userA)
