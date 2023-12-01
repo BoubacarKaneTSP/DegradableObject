d387791f7b313a08e0ff1bddaf943985a217f9a1
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-12-01 08:23:18 +0100

    test all op DAP graph with JUC and DEG

--- experiences/rm_file.py
+++ experiences/rm_file.py
@@ -8,7 +8,7 @@ completion_time = sys.argv[3]
 if completion_time ==  "True":
     list_op = ["ALL"]
 else:
-    list_op = ["ALL", "ADD", "FOLLOW", "UNFOLLOW", "TWEET", "READ", "COUNT","GROUPE","PROFILE", "avg_queue_size", "avg_Follower", "avg_Following", "proportion_Max_Follower", "proportion_Max_Following","proportion_User_With_Max_Following", "proportion_User_With_Max_Follower", "proportion_User_Without_Follower","proportion_User_Without_Following", "nb_user_final", "nb_tweet_final", "avg_time_computed"]
+    list_op = ["ALL", "ADD", "FOLLOW", "UNFOLLOW", "TWEET", "READ", "COUNT","GROUPE","PROFILE","avg_time_computed"]#, "avg_queue_size", "avg_Follower", "avg_Following", "proportion_Max_Follower", "proportion_Max_Following","proportion_User_With_Max_Following", "proportion_User_With_Max_Follower", "proportion_User_Without_Follower","proportion_User_Without_Following", "nb_user_final", "nb_tweet_final"]
 
 for op in list_op:
     os.remove(op+"_"+type_obj+"_"+nb_user+".txt")
