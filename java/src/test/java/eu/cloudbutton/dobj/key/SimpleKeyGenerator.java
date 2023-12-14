77fb7b71af82a888b115f01920eb72c961599ded
otrack <0track@gmail.com>
2023-12-14 09:12:25 +0100

    update

--- java/src/test/java/eu/cloudbutton/dobj/key/SimpleKeyGenerator.java
+++ java/src/test/java/eu/cloudbutton/dobj/key/SimpleKeyGenerator.java
@@ -5,17 +5,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SimpleKeyGenerator implements KeyGenerator {
 
-    private ThreadLocal<Random> random;
     private int max_key_per_thread;
 
     public SimpleKeyGenerator(int max_key_per_thread) {
-        random = ThreadLocal.withInitial(() -> new Random(System.nanoTime()+Thread.currentThread().getId()));
         this.max_key_per_thread = max_key_per_thread;
     }
 
     @Override
     public Key nextKey() {
-        int nextInt = random.get().nextInt(Integer.MAX_VALUE);
+        int nextInt = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
         long id = nextInt < 0 ? 0 : nextInt;
         return new ThreadLocalKey(Thread.currentThread().getId(), id, max_key_per_thread);
     }
