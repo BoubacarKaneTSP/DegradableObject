6a430989eb51dc83524b6b47eeb89b82868d2d32
otrack <0track@gmail.com>
2023-12-14 09:20:41 +0100

    update

--- java/src/test/bin/simple.sh
+++ java/src/test/bin/simple.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CLASSPATH=target/degradableobject-1.0.jar:target/degradableobject-1.0-tests.jar:target/lib/*
+export CLASSPATH="target/degradableobject-1.0.jar:target/degradableobject-1.0-tests.jar:target/lib/*"
 
 type=$1
 time=$2
 nbThreads=$3
 nbOps=$4
 
-java -Xlog:gc -XX:+UseNUMA -XX:+UseG1GC -XX:-RestrictContended -ea \
+java -Xlog:gc -XX:+UseNUMA -XX:+UseG1GC -XX:-RestrictContended -ea --enable-preview \
 eu.cloudbutton.dobj.benchmark.Microbenchmark \
 -type $type \
 -nbThreads $nbThreads \
