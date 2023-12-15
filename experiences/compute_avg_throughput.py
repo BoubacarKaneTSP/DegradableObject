f5fb7472897f1a2f1b18a8f8be190c96e4450326
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-12-15 12:51:58 +0100

    testing Retwis with uniform usage distribution ExtHashMapNoCleanTL for Profile

--- experiences/compute_avg_throughput.py
+++ experiences/compute_avg_throughput.py
@@ -23,7 +23,7 @@ completion_time = sys.argv[4]
 
 list_nb_thread = list_nb_thread.split(" ")
 
-tag_spe = "_ExtConcHashMapNoCleanTL"
+tag_spe = "_ExtHashMapNoCleanTL"
 
 if completion_time == "True":
     list_op = ["ALL"]
