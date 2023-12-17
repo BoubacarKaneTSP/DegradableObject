30d17bc24b1b1a6247922f7d66de4a1553d8d249
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-12-17 16:27:11 +0100

    testing Retwis with uniform usage distribution ExtHashNoCleanTL for Groupe (10k users)

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Database.java
@@ -96,10 +96,10 @@ public class Database {
 //            community = Factory.createSet(typeSet, nbThread);
 //        }
 
-//        community = new ExtendedSegmentedHashSet(factoryIndice);
 //        mapProfiles = new ExtendedSegmentedHashMap<>(factoryIndice);
-        community = new ConcurrentHashSet<>();
         mapProfiles = new ConcurrentHashMap<>();
+//        community = new ConcurrentHashSet<>();
+        community = new ExtendedSegmentedHashSet(factoryIndice);
 
         usersFollowProbability = new ConcurrentSkipListMap<>();
         localUsersUsageProbability = new ConcurrentHashMap<>();
