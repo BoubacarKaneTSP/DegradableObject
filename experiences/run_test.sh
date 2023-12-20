1cc98ab9197d77b1f3cedf3587feacf941784db8
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2023-12-20 16:09:06 +0100

    test

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,11 +6,12 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 initSize=16384
 range=32768
-nbTest=10
+nbTest=1
 benchmarkTime=60
 warmingUpTime=20
 
-for nbThread in 1 2 4 8 16 32 48 64 70 86 96
+for nbThread in 96
+#for nbThread in 1 2 4 8 16 32 48 64 70 86 96
 do
 #  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
@@ -36,8 +37,8 @@ done
 #python3 compute_avg_throughput_microbenchmark.py "WrappedLongAdder" "1 2 4 8 16 32 48 64 70 86 96"
 #
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListSet" "1 2 4 8 16 32 48 64 70 86 96"
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 2 4 8 16 32 48 64 70 86 96"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 2 4 8 16 32 48 64 70 86 96"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 2 4 8 16 32 48 64 70 86 96"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 2 4 8 16 32 48 64 70 86 96"
 #
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentLinkedQueue" "2 4 8 16 32 48 64 70 86 96"
 #python3 compute_avg_throughput_microbenchmark.py "QueueMASP" "2 4 8 16 32 48 64 70 86 96"
