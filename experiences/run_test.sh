d1faab50e9b543684381c9f66ee80253aeb7ad95
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-02-15 13:01:22 +0100

    microbench

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,7 +6,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 initSize=16384
 range=32768
-nbTest=30
+nbTest=3
 benchmarkTime=30
 warmingUpTime=10
 
@@ -17,17 +17,17 @@ do
   perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
   perf stat -B -e cache-references,cache-misses ./test.sh -c LongAdder -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 ##
-  perf stat -B -e cache-references,cache-misses ./test.sh -s Set -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -s ConcurrentHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedSegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s Set -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s ConcurrentHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedSegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #
 #  perf stat -B -e cache-references,cache-misses ./test.sh -q Queue -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -q QueueMASP -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
 
 ##
-  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedConcurrentHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedConcurrentHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m ConcurrentSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 done
@@ -36,9 +36,9 @@ python3 compute_avg_throughput_microbenchmark.py "CounterJUC" "1 32 96"
 python3 compute_avg_throughput_microbenchmark.py "CounterIncrementOnly" "1 32 96"
 python3 compute_avg_throughput_microbenchmark.py "WrappedLongAdder" "1 32 96"
 
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListSet" "1 32 96"
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 32 96"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListSet" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 32 96"
 
 #python3 compute_avg_throughput_microbenchmark.py "CounterJUC" "1 2 4 8 16 32 48 64 70 86 96"
 #python3 compute_avg_throughput_microbenchmark.py "CounterIncrementOnly" "1 2 4 8 16 32 48 64 70 86 96"
@@ -51,9 +51,9 @@ python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 3
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentLinkedQueue" "2 4 8 16 32 48 64 70 86 96"
 #python3 compute_avg_throughput_microbenchmark.py "QueueMASP" "2 4 8 16 32 48 64 70 86 96"
 #
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 32 96"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashMap" "1 32 96"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedConcurrentHashMap" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashMap" "1 32 96"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedConcurrentHashMap" "1 32 96"
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListMap" "1 2 4 8 16 32 48 64 70 86 96"
 #python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedSkipListMap" "1 2 4 8 16 32 48 64 70 86 96"
 
