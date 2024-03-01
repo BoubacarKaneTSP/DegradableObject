a8e956793e1880cc8d36da9c6d823f322838519b
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-03-01 03:55:37 +0100

    test retwis

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,12 +6,12 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 initSize=16384
 range=32768
-nbTest=1
+nbTest=2
 benchmarkTime=60
 warmingUpTime=30
 
 #for nbThread in 1 2 4 8 16 32 48 64 70 86 96
-for nbThread in 100 500 1000
+for nbThread in 1 100 500 1000
 do
 #  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
@@ -19,14 +19,14 @@ do
 ##
 #  perf stat -B -e cache-references,cache-misses ./test.sh -s Set -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -s ConcurrentHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -s SegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s SegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedSegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #
 #  perf stat -B -e cache-references,cache-misses ./test.sh -q Queue -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -q QueueMASP -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
 
 ##
-#  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedConcurrentHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
   perf stat -B -e cache-references,cache-misses ./test.sh -m SegmentedHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
@@ -54,8 +54,8 @@ done
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentLinkedQueue" "2 4 8 16 32 48 64 70 86 96"
 #python3 compute_avg_throughput_microbenchmark.py "QueueMASP" "2 4 8 16 32 48 64 70 86 96"
 #
-#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 100 500 1000"
-python3 compute_avg_throughput_microbenchmark.py "SegmentedHashMap" "100 500 1000"
+python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 100 500 1000"
+python3 compute_avg_throughput_microbenchmark.py "SegmentedHashMap" "1 100 500 1000"
 #python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashMap" "1 100 500 1000"
 #python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedConcurrentHashMap" "1 100 500 1000"
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListMap" "1 2 4 8 16 32 48 64 70 86 96"
