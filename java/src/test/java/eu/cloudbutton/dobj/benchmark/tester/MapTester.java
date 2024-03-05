3a3e1f3b7451b2c1c4937bcda87c7273c54c50af
otrack <0track@gmail.com>
2024-03-05 16:06:18 +0100

    update

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/tester/MapTester.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/tester/MapTester.java
@@ -39,20 +39,20 @@ public class MapTester extends Tester<Map> {
             case ADD:
                 startTime = System.nanoTime();
 
-                for (int i = 0; i < nbRepeat; i++) {
-                    int finalI = i;
-                    object.compute(list.get(i), (k, v) -> {
-                        int p = 0;
-                        for(int j=0;j<=nbRepeat; j++) {
-                            p+=j;
-                        }
-                        return finalI;
-                    });
-                }
-
 //                for (int i = 0; i < nbRepeat; i++) {
-//                    object.put(list.get(i),i);
+//                    int finalI = i;
+//                    object.compute(list.get(i), (k, v) -> {
+//                        int p = 0;
+//                        for(int j=0;j<=nbRepeat; j++) {
+//                            p+=j;
+//                        }
+//                        return finalI;
+//                    });
 //                }
+
+                for (int i = 0; i < nbRepeat; i++) {
+                    object.put(list.get(i),i);
+                }
                 endTime = System.nanoTime();
                 break;
             case REMOVE:
