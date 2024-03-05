7ce4d22523091a28269088cc4baf49124bf2444e
otrack <0track@gmail.com>
2024-03-05 15:19:00 +0100

    update

--- java/src/main/java/eu/cloudbutton/dobj/utils/BaseSegmentation.java
+++ java/src/main/java/eu/cloudbutton/dobj/utils/BaseSegmentation.java
@@ -38,7 +38,7 @@ public class BaseSegmentation<T> implements Segmentation<T> {
         // int index = carrierID();
 
         if (!redirect.containsKey(index)) {
-            System.out.println("Index: " + index);
+            // System.out.println("Index: " + index);
             try {
                 T ret = this.clazz.getDeclaredConstructor().newInstance();
                 this.segments.add(ret);
