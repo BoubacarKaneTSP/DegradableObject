fa6de4099166c03342a32ef9408eed98f8ed26ef
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-03-27 19:53:16 +0000

    resuts

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -15,49 +15,49 @@ warmingUpTime=30
 for nbThread in 1 40 80 120 160
 #for nbThread in 1 80
 do
-  perf stat -B -e cache-references,cache-misses ./test.sh -c AtomicReference -t Microbenchmark -p -e -r "0 100 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -c AtomicWriteOnceReference -t Microbenchmark -p -e -r "0 100 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+  perf stat -B -e cache-references,cache-misses ./test.sh -c AtomicReference -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+  perf stat -B -e cache-references,cache-misses ./test.sh -c AtomicWriteOnceReference -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 
-  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -c LongAdder -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterJUC -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -c CounterIncrementOnly -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -c LongAdder -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 ##
 #  perf stat -B -e cache-references,cache-misses ./test.sh -s Set -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -s ConcurrentHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s ConcurrentHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -s SegmentedHashSet -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedSegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedSegmentedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #
-  perf stat -B -e cache-references,cache-misses ./test.sh -q Queue -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -q QueueMASP -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -q Queue -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -q QueueMASP -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -a -d $range -g $nbThread
 
 ##
-  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m Map -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedConcurrentHashMap -t Microbenchmark -p -e -r "100 0 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 #  perf stat -B -e cache-references,cache-misses ./test.sh -m SegmentedHashMap -t Microbenchmark -p -e -r "0 0 100" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedHashMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -m ConcurrentSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
-  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedHashMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m ConcurrentSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
+#  perf stat -B -e cache-references,cache-misses ./test.sh -m ExtendedSegmentedSkipListMap -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range -g $nbThread
 done
 
 python3 compute_avg_throughput_microbenchmark.py "AtomicReference" "1 40 80 120 160"
 python3 compute_avg_throughput_microbenchmark.py "AtomicWriteOnceReference" "1 40 80 120 160"
 
-python3 compute_avg_throughput_microbenchmark.py "CounterJUC" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "CounterIncrementOnly" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "WrappedLongAdder" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "CounterJUC" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "CounterIncrementOnly" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "WrappedLongAdder" "1 40 80 120 160"
 
 #python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListSet" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashSet" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashSet" "1 40 80 120 160"
 #
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentLinkedQueue" "2 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "QueueMASP" "2 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentLinkedQueue" "2 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "QueueMASP" "2 40 80 120 160"
 ##
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentHashMap" "1 40 80 120 160"
 #python3 compute_avg_throughput_microbenchmark.py "SegmentedHashMap" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashMap" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListMap" "1 40 80 120 160"
-python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedSkipListMap" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedHashMap" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ConcurrentSkipListMap" "1 40 80 120 160"
+#python3 compute_avg_throughput_microbenchmark.py "ExtendedSegmentedSkipListMap" "1 40 80 120 160"
 
 #perf stat -B -e cache-references,cache-misses ./test.sh -s SegmentedSkipListSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range
 #perf stat -B -e cache-references,cache-misses ./test.sh -s ExtendedShardedHashSet -t Microbenchmark -p -e -r "50 50 0" -w $benchmarkTime -u $warmingUpTime -n $nbTest -i $initSize -d $range
