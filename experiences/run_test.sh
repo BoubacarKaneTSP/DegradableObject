3a5b3f75ab95bff5467dc99932a640277cc732aa
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-04-02 04:15:15 +0200

    test map ratio 75 and init size 65k

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -4,8 +4,8 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-initSize=32768
-range=65536
+initSize=65536
+range=131072
 nbTest=5
 benchmarkTime=60
 warmingUpTime=30
