951b319182c2054619908ff011543339cbfbc483
otrack <0track@gmail.com>
2024-04-10 11:40:37 +0200

    update

--- java/src/main/java/eu/cloudbutton/dobj/Timeline.java
+++ java/src/main/java/eu/cloudbutton/dobj/Timeline.java
@@ -25,21 +25,22 @@ public class Timeline<T> {
 
    public Queue<T> read() throws InterruptedException {
        long queueSize = timeline.size();
-       if (queueSize>10_000) {
-           System.out.println(queueSize);
-       }
        for (int i = 0; i < queueSize; i++)
            topk.add(timeline.poll());
-
-       int topkSize = topk.size();
-       if (topkSize>10_000) {
-           System.out.println(topkSize);
+       if (queueSize>10_000) {
+           System.out.println(queueSize);
+           assert queueSize == topk.size();
        }
-        for (int i = 0; i < topkSize - CAPACITY; i++)
-            topk.poll();
-       if (topkSize>10_000) {
-           System.out.println("OUT");
-       }
+       topk.clear();
+//       int topkSize = topk.size();
+//       if (topkSize>10_000) {
+//           System.out.println(topkSize);
+//       }
+//        for (int i = 0; i < topkSize - CAPACITY; i++)
+//            topk.poll();
+//       if (topkSize>10_000) {
+//           System.out.println("OUT");
+//       }
        return topk;
    }
 
