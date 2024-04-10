3774c363c4abdbf3277f3aea5bbd98aefdcdae3d
otrack <0track@gmail.com>
2024-04-10 14:07:55 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
@@ -1,6 +1,5 @@
 package eu.cloudbutton.dobj.benchmark;
 
-import eu.cloudbutton.dobj.Factory;
 import eu.cloudbutton.dobj.Profile;
 import eu.cloudbutton.dobj.Timeline;
 import eu.cloudbutton.dobj.incrementonly.BoxedLong;
@@ -1259,50 +1258,6 @@ public class Retwis {
             printWriter.flush();
             fileWriter.close();
         }
-    }
-
-    private static void performHeapDump(String tag, String when, int nbUser) {
-        System.out.println("Performing heapDump");
-        String jcmdCommand = "jcmd";
-        String processId = getProcessId();
-
-        try {
-            Process process = Runtime.getRuntime().exec(jcmdCommand);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                // Recherche de la ligne contenant le processus Java souhaité
-                if (line.contains(processId)) {
-                    String[] tokens = line.trim().split("\\s+");
-                    String pid = tokens[0];
-                    String heapDumpCommand = "jcmd " + pid + " GC.heap_dump " + "heapdump_"+ when +"_benchmark_" + tag +"_" + nbUser +".hprof";
-
-                    // Exécution de la commande jcmd pour effectuer le heap dump
-                    Process heapDumpProcess = Runtime.getRuntime().exec(heapDumpCommand);
-
-                    // Attente de la fin de l'exécution de la commande
-                    int exitCode = heapDumpProcess.waitFor();
-
-                    if (exitCode == 0) {
-                        System.out.println("Heap dump effectué avec succès !");
-                    } else {
-                        System.out.println("Erreur lors de l'exécution de la commande jcmd.");
-                    }
-
-                    break;
-                }
-            }
-
-            reader.close();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String getProcessId() {
-        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
-        return processName.split("@")[0];
     }
 
     public void startMonitoring(){
