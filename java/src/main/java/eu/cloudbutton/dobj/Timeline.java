bbe904b67b4f2eecb32b6cfcdc3bc47c0460fb41
otrack <0track@gmail.com>
2024-04-10 10:47:46 +0200

    update

--- java/src/main/java/eu/cloudbutton/dobj/Timeline.java
+++ java/src/main/java/eu/cloudbutton/dobj/Timeline.java
@@ -38,6 +38,9 @@ public class Timeline<T> {
 
         for (int i = 0; i < topkSize - CAPACITY; i++)
             topk.poll();
+       if (topkSize>1_000_000) {
+           System.out.println("OUT");
+       }
        return topk;
    }
 
