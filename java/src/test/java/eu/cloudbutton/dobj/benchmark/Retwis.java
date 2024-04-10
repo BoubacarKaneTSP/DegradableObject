97926d871aa83840895b8f32c2af12f86fd92cc3
otrack <0track@gmail.com>
2024-04-10 09:36:02 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
@@ -1002,7 +1002,7 @@ public class Retwis {
 //
                                         startTime = System.nanoTime();
                                         typeComputed = COUNT;
-                                        for (int i = 0; i < 1000; i++) {
+                                        for (int i = 0; i < 10; i++) {
                                             // System.out.println("here w."+Thread.currentThread().getName());
                                             database.getCounter().incrementAndGet();
                                         }
