b4eb1324d43115502abddf399b0e1db459950e45
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-04-17 20:45:51 +0200

    test microbench Write only

--- experiences/run_test.sh
+++ experiences/run_test.sh
@@ -6,12 +6,12 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 initSize=16384
 range=32768
-nbTest=5
+nbTest=10
 benchmarkTime=60
 warmingUpTime=30
-nbThreads=("1" "10" "20" "40")
-objects=("ConcurrentHashMap" "ExtendedSegmentedHashMap")
-ratio="37 37 26"
+nbThreads=("1" "5" "10" "20" "40" "80")
+objects=("ConcurrentHashMap" "ExtendedSegmentedHashMap" "ConcurrentSkipListMap" "ExtendedSegmentedSkipListMap" "CounterJUC" "CounterIncrementOnly" "LongAdder")
+ratio="100 0 0"
 
 
 #for nbThread in 80
