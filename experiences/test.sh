764a8f07411d4ce10d90533416d33b59982a50c8
otrack <0track@gmail.com>
2024-05-02 16:52:50 +0200

    update

--- experiences/test.sh
+++ experiences/test.sh
@@ -35,146 +35,146 @@ alpha=""
 compile=false
 
 while getopts 'xc:s:q:l:m:t:r:pew:u:n:fakvoi:zy:bh:g:d:jA:' OPTION; do
-  case "$OPTION" in
-    x)
-      mvn clean package -f ../java -DskipTests;
-      compile=true
-      ;;
-    A)
-      alpha="$OPTARG"
-      ;;
-    c)
-      typeCounter="$OPTARG"
-      ;;
-    s)
-      typeSet="$OPTARG"
-      ;;
-    q)
-      typeQueue="$OPTARG"
-      ;;
-    l)
-      typeList="$OPTARG"
-      ;;
-    m)
-      typeMap="$OPTARG"
-      ;;
-    t)
-      typeTest="$OPTARG"
+    case "$OPTION" in
+	x)
+	    mvn clean package -f ../java -DskipTests;
+	    compile=true
+	    ;;
+	A)
+	    alpha="$OPTARG"
+	    ;;
+	c)
+	    typeCounter="$OPTARG"
+	    ;;
+	s)
+	    typeSet="$OPTARG"
+	    ;;
+	q)
+	    typeQueue="$OPTARG"
+	    ;;
+	l)
+	    typeList="$OPTARG"
+	    ;;
+	m)
+	    typeMap="$OPTARG"
+	    ;;
+	t)
+	    typeTest="$OPTARG"
 
-      if [[ $typeTest == "Microbenchmark" ]]
-      then
-         sum=0
-          if [[ $typeCounter != "" ]]
-          then
-            ((sum+=1))
-            type=$typeCounter
-          fi
+	    if [[ $typeTest == "Microbenchmark" ]]
+	    then
+		sum=0
+		if [[ $typeCounter != "" ]]
+		then
+		    ((sum+=1))
+		    type=$typeCounter
+		fi
 
-          if [[ $typeSet != "" ]]
-          then
-            ((sum+=1))
-            type=$typeSet
-          fi
+		if [[ $typeSet != "" ]]
+		then
+		    ((sum+=1))
+		    type=$typeSet
+		fi
 
-          if [[ $typeList != "" ]]
-          then
-            ((sum+=1))
-            type=$typeList
-          fi
+		if [[ $typeList != "" ]]
+		then
+		    ((sum+=1))
+		    type=$typeList
+		fi
 
-          if [[ $typeQueue != "" ]]
-          then
-            ((sum+=1))
-            type=$typeQueue
-          fi
+		if [[ $typeQueue != "" ]]
+		then
+		    ((sum+=1))
+		    type=$typeQueue
+		fi
 
-          if [[ $typeMap != "" ]]
-          then
-            ((sum+=1))
-            type=$typeMap
-          fi
+		if [[ $typeMap != "" ]]
+		then
+		    ((sum+=1))
+		    type=$typeMap
+		fi
 
-          if [[ ! ($sum -eq 1) ]]
-          then
-            echo "One type must be specified in order to run the micro-Benchmark. (Before test specification)" >&2
-            exit 1
-          fi
+		if [[ ! ($sum -eq 1) ]]
+		then
+		    echo "One type must be specified in order to run the micro-Benchmark. (Before test specification)" >&2
+		    exit 1
+		fi
 
-          echo "The type being tested is : $type"
+		echo "The type being tested is : $type"
 
-      elif [[ $typeTest == "Retwis" ]]
-      then
-        if [[ $typeCounter == "" ]] && [[ $typeSet == "" ]] && [[ $typeQueue == "" ]] && [[ $typeMap == "" ]]
-        then
-          echo "Must be specified a type for : a Counter, a Set, a Queue and a Map in order to run the Retwis Benchmark (Before test specification)" >&2
-            exit 1
-        fi
-        echo "The counter used is : $typeCounter"
-        echo "The set used is : $typeSet"
-        echo "The queue used is : $typeQueue"
-        echo "The map used is : $typeMap"
-      else
-        echo "Test type must be Microbenchmark or Retwis." >&2
-        exit 1
-      fi
-      ;;
-    r)
-      ratio="$OPTARG"
-      ;;
-    p)
-      print="-p"
-      ;;
-    e)
-      save="-s"
-      ;;
-    w)
-      workloadTime="-time $OPTARG"
-      ;;
-    u)
-      warmingUpTime="-wTime $OPTARG"
-      ;;
-    n)
-      nbTest="$OPTARG"
-      ;;
-    f)
-      printFail="-ratioFail"
-      ;;
-    a)
-      asymmetric="-asymmetric"
-      ;;
-    k)
-      collisionKey="-collisionKey"
-      ;;
-    v)
-      quickTest="-quickTest"
-      ;;
-    i)
-      nbInitialAdd="-nbOps $OPTARG"
-      ;;
-    z)
-      completionTime="-completionTime"
-      ;;
-    y)
-      nbUserInit="-nbUserInit $OPTARG"
-      nbUser="$OPTARG"
-      ;;
-    b)
-      breakdown="-breakdown"
-      ;;
-    h)
-      tag="$OPTARG"
-      ;;
-    g)
-      nbThreads="-nbThreads $OPTARG"
-      ;;
-    d)
-      nbItemsPerThread="-nbItems $OPTARG"
-      ;;
-    j)
-      computeGCInfo=true
-      ;;
-    o)
-      echo "script usage: $(basename \$0)
+	    elif [[ $typeTest == "Retwis" ]]
+	    then
+		if [[ $typeCounter == "" ]] && [[ $typeSet == "" ]] && [[ $typeQueue == "" ]] && [[ $typeMap == "" ]]
+		then
+		    echo "Must be specified a type for : a Counter, a Set, a Queue and a Map in order to run the Retwis Benchmark (Before test specification)" >&2
+		    exit 1
+		fi
+		echo "The counter used is : $typeCounter"
+		echo "The set used is : $typeSet"
+		echo "The queue used is : $typeQueue"
+		echo "The map used is : $typeMap"
+	    else
+		echo "Test type must be Microbenchmark or Retwis." >&2
+		exit 1
+	    fi
+	    ;;
+	r)
+	    ratio="$OPTARG"
+	    ;;
+	p)
+	    print="-p"
+	    ;;
+	e)
+	    save="-s"
+	    ;;
+	w)
+	    workloadTime="-time $OPTARG"
+	    ;;
+	u)
+	    warmingUpTime="-wTime $OPTARG"
+	    ;;
+	n)
+	    nbTest="$OPTARG"
+	    ;;
+	f)
+	    printFail="-ratioFail"
+	    ;;
+	a)
+	    asymmetric="-asymmetric"
+	    ;;
+	k)
+	    collisionKey="-collisionKey"
+	    ;;
+	v)
+	    quickTest="-quickTest"
+	    ;;
+	i)
+	    nbInitialAdd="-nbOps $OPTARG"
+	    ;;
+	z)
+	    completionTime="-completionTime"
+	    ;;
+	y)
+	    nbUserInit="-nbUserInit $OPTARG"
+	    nbUser="$OPTARG"
+	    ;;
+	b)
+	    breakdown="-breakdown"
+	    ;;
+	h)
+	    tag="$OPTARG"
+	    ;;
+	g)
+	    nbThreads="-nbThreads $OPTARG"
+	    ;;
+	d)
+	    nbItemsPerThread="-nbItems $OPTARG"
+	    ;;
+	j)
+	    computeGCInfo=true
+	    ;;
+	o)
+	    echo "script usage: $(basename \$0)
       [-a] Test an asymmetrical workload,
       [-b] Print the details results for all operations,
       [-c] counter type,
@@ -201,10 +201,10 @@ while getopts 'xc:s:q:l:m:t:r:pew:u:n:fakvoi:zy:bh:g:d:jA:' OPTION; do
       [-x] compile the project,
       [-y] Number of initial user in Retwis,
       [-z] Computing the completionTime for Retwis">&2
-      exit 1
-      ;;
-    ?)
-      echo "script usage: $(basename \$0)
+	    exit 1
+	    ;;
+	?)
+	echo "script usage: $(basename \$0)
       [-a] Test an asymmetrical workload,
       [-b] Print the details results for all operations,
       [-c] counter type,
@@ -231,9 +231,9 @@ while getopts 'xc:s:q:l:m:t:r:pew:u:n:fakvoi:zy:bh:g:d:jA:' OPTION; do
       [-x] compile the project,
       [-y] Number of initial user in Retwis,
       [-z] Computing the completionTime for Retwis">&2
-      exit 1
-      ;;
-  esac
+	exit 1
+	;;
+    esac
 done
 
 cpuIDs=""
@@ -249,20 +249,20 @@ echo ${min}
 val=0
 
 if [ "$compile" = false ]; then
-  for range in "${ranges[@]}"; do
-      start=$(echo "$range" | cut -d'-' -f1)
-      end=$(echo "$range" | cut -d'-' -f2)
-      for ((i=start; i<=end; i++)); do
-        if [ "$val" -lt "$min" ]; then
-          if [ -n "$cpuIDs" ]; then
-            cpuIDs="$cpuIDs,$i"
-          else
-            cpuIDs="$i"
-          fi
-        fi
-        ((val += 1))
-      done
-  done
+    for range in "${ranges[@]}"; do
+	start=$(echo "$range" | cut -d'-' -f1)
+	end=$(echo "$range" | cut -d'-' -f2)
+	for ((i=start; i<=end; i++)); do
+            if [ "$val" -lt "$min" ]; then
+		if [ -n "$cpuIDs" ]; then
+		    cpuIDs="$cpuIDs,$i"
+		else
+		    cpuIDs="$i"
+		fi
+            fi
+            ((val += 1))
+	done
+    done
 fi
 
 echo "The test launched is : $typeTest"
@@ -295,24 +295,26 @@ JVM_ARGS="-Xms5g -Xmx16g -XX:+UseNUMA -XX:+UseG1GC -XX:-RestrictContended -ea ${
 
 RETWIS_ARGS="-set $typeSet -queue $typeQueue -counter $typeCounter -map $typeMap -distribution $ratio -nbTest $nbTest $nbThreads $workloadTime $warmingUpTime $nbInitialAdd $completionTime $nbUserInit $print $save $breakdown $quickTest $collisionKey $nbItemsPerThread -tag $tag -gcinfo -alphaMin ${alpha}"
 
+NUMA="numactl --physcpubind=$cpuIDs --membind=0"
+
 if [[ $typeTest == "Microbenchmark" ]]
 then
-  if [[ $computeGCInfo == true ]]
-  then
-    CLASSPATH=../java/target/*:../java/target/lib/* numactl --physcpubind="$cpuIDs" --membind=0 java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Microbenchmark -type $type -ratios $ratio -nbTest $nbTest $nbThreads $workloadTime $warmingUpTime $nbInitialAdd $print $save $printFail $asymmetric $collisionKey $quickTest $nbItemsPerThread -gcinfo | egrep "nbThread|benchmarkAvgTime|Start benchmark|End benchmark|G1 Evacuation Pause" > "$type"_gcinfo.log
-    python3 analyse_gc.py $type $nbTest "$nbUserInit"
-  else
-    CLASSPATH=../java/target/*:../java/target/lib/* numactl --physcpubind="$cpuIDs" --membind=0 java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Microbenchmark -type $type -ratios $ratio -nbTest $nbTest $nbThreads $workloadTime $warmingUpTime $nbInitialAdd $print $save $printFail $asymmetric $collisionKey $quickTest $nbItemsPerThread
-  fi
+    if [[ $computeGCInfo == true ]]
+    then
+	CLASSPATH=../java/target/*:../java/target/lib/* numactl --physcpubind="$cpuIDs" --membind=0 java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Microbenchmark -type $type -ratios $ratio -nbTest $nbTest $nbThreads $workloadTime $warmingUpTime $nbInitialAdd $print $save $printFail $asymmetric $collisionKey $quickTest $nbItemsPerThread -gcinfo | egrep "nbThread|benchmarkAvgTime|Start benchmark|End benchmark|G1 Evacuation Pause" > "$type"_gcinfo.log
+	python3 analyse_gc.py $type $nbTest "$nbUserInit"
+    else
+	CLASSPATH=../java/target/*:../java/target/lib/* numactl --physcpubind="$cpuIDs" --membind=0 java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Microbenchmark -type $type -ratios $ratio -nbTest $nbTest $nbThreads $workloadTime $warmingUpTime $nbInitialAdd $print $save $printFail $asymmetric $collisionKey $quickTest $nbItemsPerThread
+    fi
 elif [[ $typeTest == "Retwis" ]]
 then
-  if [[ $computeGCInfo == true ]]
-  then
-      CLASSPATH=../java/target/*:../java/target/lib/* numactl --physcpubind=$cpuIDs --membind=0 java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Retwis ${RETWIS_ARGS} | egrep "nbThread|benchmarkAvgTime|Start benchmark|End benchmark|G1 Evacuation Pause" > "$tag"_gcinfo.log
-    python3 analyse_gc.py $tag $nbTest $nbUserInit
-  else
-      CLASSPATH=../java/target/*:../java/target/lib/* java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Retwis ${RETWIS_ARGS}
-      echo ${cmd}
-      eval ${cmd}
-  fi
+    if [[ $computeGCInfo == true ]]
+    then
+	CLASSPATH=../java/target/*:../java/target/lib/* ${NUMA} java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Retwis ${RETWIS_ARGS} | egrep "nbThread|benchmarkAvgTime|Start benchmark|End benchmark|G1 Evacuation Pause" > "$tag"_gcinfo.log
+	python3 analyse_gc.py $tag $nbTest $nbUserInit
+    else
+	CLASSPATH=../java/target/*:../java/target/lib/* ${NUMA} java ${JVM_ARGS} eu.cloudbutton.dobj.benchmark.Retwis ${RETWIS_ARGS}
+	echo ${cmd}
+	eval ${cmd}
+    fi
 fi
