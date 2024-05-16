999862a559c48a45c3182eda7d8648ca9c9e4ed1
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-16 21:03:25 +0200

    update

--- java/src/main/java/eu/cloudbutton/dobj/asymmetric/swmr/map/SWMRHashMap.java
+++ java/src/main/java/eu/cloudbutton/dobj/asymmetric/swmr/map/SWMRHashMap.java
@@ -238,8 +238,8 @@ public class SWMRHashMap<K,V> extends AbstractMap<K,V>
     /**
      * The default initial capacity - MUST be a power of two.
      */
-//    static final int DEFAULT_INITIAL_CAPACITY = 1 << 20; // aka 16
-    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 20; // aka 16
+//    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
 
     /**
      * The maximum capacity, used if a higher value is implicitly specified
