682f582036dc3390b3f90445c6c7b7176664aa00
otrack <0track@gmail.com>
2024-05-20 17:31:36 +0000

    update

--- experiences/test_alpha.sh
+++ experiences/test_alpha.sh
@@ -24,32 +24,33 @@ nbOps=10000000
 # ratio="40 60 0 0 0 0"
 # ratio="0 100 0 0 0 0"
 # ratio="100 0 0 0 0 0"
-ratio="10 15 15 50 5 5"
+# ratio="5 5 15 60 5 10"
+ratio="5 15 30 50 0 0"
 
 # alphas=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "1.7" "1.9" "2")
 # alphas=("0.01")
 # alphas=("1")
-alphas=("0.01" "1")
+# alphas=("0.01" "1")
+alphas=("0.1" "1")
 
 declare -A params
 params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
 params[dego]="-c CounterIncrementOnly -s SegmentedHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
 
-for alpha in "${alphas[@]}";
+for impl in juc dego;
 do
-    str_alpha=$(echo "$alpha" | tr '.' '-')
-    for nbUsersInit in 1000 100000 1000000
+    for alpha in "${alphas[@]}";
     do
-	for nbThread in 1 5 10 20 40 # 80 # 2 10 # 12
+	for nbUsersInit in 100000 500000 1000000
 	do
-	    for impl in juc dego;
+	    for nbThread in 1 5 10 20 40 80 
 	    do
-		nbOps=$((100000*nbThread))
+		nbOps=$((1000000*nbThread))
 		for (( c=1; c<=nbTest; c++ ))
 		do
 		    # perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
-		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
-		    echo ${impl}";"${nbUsersInit}";"${nbThread}";"${perf}
+ 		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+		    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf}
 		    #  perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
 		    # ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
 		    # ./test.sh -c CounterIncrementOnly -s SegmentedHashSet -q Queue -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
