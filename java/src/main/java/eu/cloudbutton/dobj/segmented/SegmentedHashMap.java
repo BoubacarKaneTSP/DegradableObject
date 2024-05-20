4a0b4064232a48776279628fd32f9fbd103e9e65
otrack <0track@gmail.com>
2024-05-20 10:18:33 +0200

    update

--- java/src/main/java/eu/cloudbutton/dobj/segmented/SegmentedHashMap.java
+++ java/src/main/java/eu/cloudbutton/dobj/segmented/SegmentedHashMap.java
@@ -3,10 +3,10 @@ package eu.cloudbutton.dobj.segmented;
 import eu.cloudbutton.dobj.asymmetric.swmr.map.SWMRHashMap;
 import eu.cloudbutton.dobj.juc.ConcurrentHashMap;
 
-public class SegmentedHashMap<K,V> extends SegmentedMap<ConcurrentHashMap,K,V> {
+public class SegmentedHashMap<K,V> extends SegmentedMap<SWMRHashMap,K,V> {
 
     public SegmentedHashMap() {
-        super(ConcurrentHashMap.class);
+        super(SWMRHashMap.class);
     }
 
 }
