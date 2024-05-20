8ed867531c92616b7d7573a6f027a5f755744fe5
otrack <0track@gmail.com>
2024-05-20 16:09:45 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
@@ -394,7 +394,7 @@ public class Retwis {
 
     public class RetwisApp implements Callable<Void>{
 
-        private static final int MAX_USERS_PER_THREAD = 10_000;
+        private static final int MAX_USERS_PER_THREAD = 100_000;
         private static final int MAX_DUMMY_USERS_PER_THREAD = 10_000;
         private static final int MAX_USERS_TO_FOLLOW_PER_THREAD = 10_000;
 
