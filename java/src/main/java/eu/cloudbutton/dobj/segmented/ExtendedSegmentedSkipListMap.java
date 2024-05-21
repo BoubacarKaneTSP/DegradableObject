57e4a9c0c88e0b20efd0c1072666566a2612af0a
otrack <0track@gmail.com>
2024-05-21 18:47:15 +0200

    update

--- java/src/main/java/eu/cloudbutton/dobj/segmented/ExtendedSegmentedSkipListMap.java
+++ java/src/main/java/eu/cloudbutton/dobj/segmented/ExtendedSegmentedSkipListMap.java
@@ -3,13 +3,12 @@ package eu.cloudbutton.dobj.segmented;
 import eu.cloudbutton.dobj.asymmetric.swmr.map.SWMRSkipListMap;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentSkipListMap;
 
 // FIXME this should be ordered
-public class ExtendedSegmentedSkipListMap<K,V> extends ExtendedSegmentedMap<ConcurrentSkipListMap,K,V>  implements Map<K,V> {
+public class ExtendedSegmentedSkipListMap<K,V> extends ExtendedSegmentedMap<SWMRSkipListMap,K,V>  implements Map<K,V> {
 
     public ExtendedSegmentedSkipListMap() {
-        super(ConcurrentSkipListMap.class);
+        super(SWMRSkipListMap.class);
     }
 
 }
