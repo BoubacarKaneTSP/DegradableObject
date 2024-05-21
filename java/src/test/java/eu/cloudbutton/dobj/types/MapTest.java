8a4e5f13a50747c85d2d63839de7e53f71bdfc33
otrack <0track@gmail.com>
2024-05-21 18:45:28 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/types/MapTest.java
+++ java/src/test/java/eu/cloudbutton/dobj/types/MapTest.java
@@ -20,7 +20,7 @@ import static org.testng.Assert.*;
 
 public class MapTest {
     private static final int MAX_ITEMS_PER_THREAD = Integer.MAX_VALUE;
-    private static final int ITEMS_PER_THREAD = 1_000;
+    private static final int ITEMS_PER_THREAD = 10_000;
 
     private Factory factory;
     private SimpleKeyGenerator generator;
@@ -105,87 +105,87 @@ public class MapTest {
         assertEquals(nbElement.get(), ITEMS_PER_THREAD * parallelism, "All elements are not successfully added");
         assertEquals(nbElement.get(), map.size(), "Error with map.size()");
 
-//        System.out.println("Test remove");
-//        Map<Integer, List<Key>> keys2 = generator.generateAndSplit(
-//                ITEMS_PER_THREAD*parallelism, parallelism);
-//        CountDownLatch latch2 = new CountDownLatch(parallelism);
-//        callable = () -> {
-//            try {
-//                latch2.countDown();
-//                latch2.await();
-//                Collection<Key> collection = keys2.get(Helpers.threadIndexInPool());
-//                for (Key key : collection) {
-//                    map.remove(key);
-//                    assertEquals(map.containsKey(key), false);
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        };
-//        executeAll(callable);
-//        System.out.println("Done");
-//
-//        assertEquals(map.size(), ITEMS_PER_THREAD * parallelism); // Should be false ?
-//        map.clear();
-//        assertEquals(map.size(), 0);
-//
-//        System.out.println("Each thread can see what other thread has added");
-//        Map<Integer, List<Key>> keys3 = generator.generateAndSplit(
-//                ITEMS_PER_THREAD * parallelism / 50, parallelism);
-//
-//        Set<Key> other = new ConcurrentSkipListSet<>();
-//        CountDownLatch latch3 = new CountDownLatch(parallelism);
-//        callable = () -> {
-//            try {
-//                latch3.countDown();
-//                latch3.await();
-//                Collection<Key> collection = keys3.get(Helpers.threadIndexInPool());
-//                for (Key key : collection) {
-//                    map.put(key,key);
-//                    other.add(key);
-//                    other.stream().forEach(x->assertTrue(map.containsKey(x)));
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        };
-//        executeAll(callable);
-//        System.out.println("Done");
-//
-//        System.out.println("Check if previous values are added");
-//        Map<Key,Key> previous = new ConcurrentHashMap<>();
-//        Map<Integer, List<Key>> keys4 = generator.generateAndSplit(
-//                100 * parallelism, parallelism);
-//
-//        CountDownLatch latch4 = new CountDownLatch(parallelism);
-//        callable = () -> {
-//            latch4.countDown();
-//            latch4.await();
-//            List<Key> collection = keys4.get(Helpers.threadIndexInPool());
-//            Key c, p = collection.get(0);
-//            map.put(p,"");
-//            try {
-//                for (int i = 1; i < collection.size(); i++) {
-//                    c = collection.get(i);
-//                    map.put(c,"");
-//                    previous.put(c,p);
-//                    p = c;
-//                    previous.keySet().stream().
-//                            forEach(x->
-//                            {
-//                                Key y = previous.get(x);
-//                                assertTrue(map.containsKey(y));
-//                            });
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        };
-//        executeAll(callable);
-//        System.out.println("done");
+        System.out.println("Test remove");
+        Map<Integer, List<Key>> keys2 = generator.generateAndSplit(
+                ITEMS_PER_THREAD*parallelism, parallelism);
+        CountDownLatch latch2 = new CountDownLatch(parallelism);
+        callable = () -> {
+            try {
+                latch2.countDown();
+                latch2.await();
+                Collection<Key> collection = keys2.get(Helpers.threadIndexInPool());
+                for (Key key : collection) {
+                    map.remove(key);
+                    assertEquals(map.containsKey(key), false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+        executeAll(callable);
+        System.out.println("Done");
+
+        assertEquals(map.size(), ITEMS_PER_THREAD * parallelism); // Should be false ?
+        map.clear();
+        assertEquals(map.size(), 0);
+
+        System.out.println("Each thread can see what other thread has added");
+        Map<Integer, List<Key>> keys3 = generator.generateAndSplit(
+                ITEMS_PER_THREAD * parallelism / 50, parallelism);
+
+        Set<Key> other = new ConcurrentSkipListSet<>();
+        CountDownLatch latch3 = new CountDownLatch(parallelism);
+        callable = () -> {
+            try {
+                latch3.countDown();
+                latch3.await();
+                Collection<Key> collection = keys3.get(Helpers.threadIndexInPool());
+                for (Key key : collection) {
+                    map.put(key,key);
+                    other.add(key);
+                    other.stream().forEach(x->assertTrue(map.containsKey(x)));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+        executeAll(callable);
+        System.out.println("Done");
+
+        System.out.println("Check if previous values are added");
+        Map<Key,Key> previous = new ConcurrentHashMap<>();
+        Map<Integer, List<Key>> keys4 = generator.generateAndSplit(
+                100 * parallelism, parallelism);
+
+        CountDownLatch latch4 = new CountDownLatch(parallelism);
+        callable = () -> {
+            latch4.countDown();
+            latch4.await();
+            List<Key> collection = keys4.get(Helpers.threadIndexInPool());
+            Key c, p = collection.get(0);
+            map.put(p,"");
+            try {
+                for (int i = 1; i < collection.size(); i++) {
+                    c = collection.get(i);
+                    map.put(c,"");
+                    previous.put(c,p);
+                    p = c;
+                    previous.keySet().stream().
+                            forEach(x->
+                            {
+                                Key y = previous.get(x);
+                                assertTrue(map.containsKey(y));
+                            });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+        executeAll(callable);
+        System.out.println("done");
 
     }
 }
