68e77bdeb1fa3a9d06f7a5dc0604348ff8e3d84c
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-28 22:07:27 +0200

    test DAP

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
@@ -445,12 +445,17 @@ public class Retwis {
                     System.out.println(myId.get()+": "+userToAdd.size()+" users");
                 assert database.getMapUserToAdd().get(myId.get()).size() > 0 : "not enough users!";
 
+                int v = 0;
 
                 for (Key user : userToAdd){
                     if (database.isDAP())
                         assert database.getMapKeyToIndice().get(user) / database.getNbUsers() != myId.get() : "Graph is not DAP";
 
+                    if (v%10000 == 0)
+                        System.out.println(v);
+
                     database.addOriginalUser(user);
+                    v++;
                 }
 
                 latchFillDatabase.countDown();
