c1f2a894ae07d2c79b04fe92261634094c7fdc2a
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-28 19:58:23 +0200

    test DAP

--- experiences/test_alpha.sh
+++ experiences/test_alpha.sh
@@ -37,9 +37,9 @@ declare -A params
 #params[seq]="-c juc.Counter -s HashSet -q LinkedList -m SkipListMap"
 params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
 params[dego]="-c CounterIncrementOnly -s SegmentedHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
-params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ShardedSkipListMap"
+params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ExtendedSegmentedSkipListMap"
 
-for impl in dego; #juc dego;
+for impl in dap; #juc dego;
 do
     for alpha in "${alphas[@]}";
     do
