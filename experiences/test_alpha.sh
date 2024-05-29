20b551221605a0aff801f11625b9ac1dfddf09ac
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-29 06:12:28 +0200

    update

--- experiences/test_alpha.sh
+++ experiences/test_alpha.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=2
+nbTest=1
 benchmarkTime=20
 warmingUpTime=20
 #nbUsersInit=1000
@@ -36,7 +36,7 @@ nbOps=10000000
 declare -A params
 #params[seq]="-c juc.Counter -s HashSet -q LinkedList -m SkipListMap"
 params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
-params[dego]="-c CounterIncrementOnly -s SegmentedHashSet -q Queue -m ConcurrentSkipListMap"
+params[dego]="-c CounterIncrementOnly -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
 #params[dego]="-c CounterIncrementOnly -s SegmentedHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
 params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ShardedSkipListMap"
 
