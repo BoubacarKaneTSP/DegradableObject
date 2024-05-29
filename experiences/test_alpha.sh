29ba8371ddfefe6b077949e3db6819251e3c40e0
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-29 16:07:54 +0200

    update

--- experiences/test_alpha.sh
+++ experiences/test_alpha.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=2
+nbTest=1
 benchmarkTime=20
 warmingUpTime=20
 #nbUsersInit=1000
@@ -40,22 +40,22 @@ params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListM
 params[dego]="-c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
 params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ShardedSkipListMap"
 
-for impl in juc dego;
+for impl in juc;# dego;
 do
     for alpha in "${alphas[@]}";
     do
 	for nbUsersInit in 100000 #500000 1000000
 	do
 #	    for nbThread in 1 5 10 20 40 80
-	    for nbThread in 1 40 80
+	    for nbThread in 1 #40 80
 	    do
 		nbOps=$((1000000*nbThread))
 		for (( c=1; c<=nbTest; c++ ))
 		do
 		    # perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
- 		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
-		    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf}
-# 		    ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
+# 		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+#		    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf}
+ 		    perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
 		    #  perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
 		    # ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
 		    # ./test.sh -c CounterIncrementOnly -s SegmentedHashSet -q Queue -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
