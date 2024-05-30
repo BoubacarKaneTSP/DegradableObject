1414af2f43634c2386ea708d34979924aa085afc
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-30 08:49:57 +0200

    update

--- experiences/test_alpha.sh
+++ experiences/test_alpha.sh
@@ -4,7 +4,7 @@
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nbTest=2
+nbTest=1
 benchmarkTime=20
 warmingUpTime=20
 #nbUsersInit=1000
@@ -17,7 +17,7 @@ nbOps=10000000
 # ratio="0 0 0 50 50 0"
 # ratio="0 0 0 0 0 100"
 # ratio="0 0 20 80 0 0"
-# ratio="0 100 0 0 0 0"
+ ratio="0 0 100 0 0 0"
 # ratio="0 0 0 0 0 100"
 # ratio="0 0 0 0 50 50"
 # ratio="0 0 1 99 0 0"
@@ -25,7 +25,7 @@ nbOps=10000000
 # ratio="0 100 0 0 0 0"
 # ratio="100 0 0 0 0 0"
 # ratio="5 5 15 60 5 10"
- ratio="5 10 0 60 10 15"
+# ratio="5 10 0 60 10 15"
 #ratio="5 15 30 50 0 0"
 
 # alphas=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "1.7" "1.9" "2")
@@ -37,7 +37,7 @@ nbOps=10000000
 declare -A params
 #params[seq]="-c juc.Counter -s HashSet -q LinkedList -m SkipListMap"
 params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
-params[dego]="-c CounterIncrementOnly -s ExtendedSegmentedHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
+params[dego]="-c CounterIncrementOnly -s ExtendedSegmentedHashSet -q Queue -m ConcurrentSkipListMap"
 #params[dego]="-c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
 params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ShardedSkipListMap"
 
@@ -54,9 +54,9 @@ do
 		for (( c=1; c<=nbTest; c++ ))
 		do
 		    # perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
- 		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
-		    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf}
-# 		    perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
+# 		    perf=$(./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+#		    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf}
+ 		    perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,l1d_pend_miss.pending_cycles_any,l2_rqsts.all_demand_references,l2_rqsts.all_demand_miss,cycle_activity.stalls_total -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "JUC_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
 		    #  perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log
 		    # ./test.sh -c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
 		    # ./test.sh -c CounterIncrementOnly -s SegmentedHashSet -q Queue -m ConcurrentSkipListMap -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "Q_M_C_$str_alpha" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z
