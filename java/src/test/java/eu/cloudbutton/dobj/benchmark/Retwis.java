038b6cc03bea802511a6cbba8fca20265543f60d
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-05-30 12:40:16 +0200

    update

--- java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
+++ java/src/test/java/eu/cloudbutton/dobj/benchmark/Retwis.java
@@ -325,8 +325,9 @@ public class Retwis {
                     if (!database.isDAP())
                         System.out.println(database.statistics());
 
-                    System.out.println("=> Nb total bin : " + database.nbTotalBin());
-                    System.out.println("=> Total memory used : " + getTotalMemoryUsed()/1_000_000 + " MB");
+                    performHeapDump();
+//                    System.out.println("=> Nb total bin : " + database.nbTotalBin());
+//                    System.out.println("=> Total memory used : " + getTotalMemoryUsed()/1_000_000 + " MB");
 
                 }
 
@@ -909,5 +910,49 @@ public class Retwis {
         long freeMemory = runtime.freeMemory();
 
         return totalMemory - freeMemory;
+    }
+
+    private void performHeapDump() {
+        System.out.println("Performing heapDump");
+        String jcmdCommand = "jcmd";
+        String processId = getProcessId();
+
+        try {
+            Process process = Runtime.getRuntime().exec(jcmdCommand);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                // Recherche de la ligne contenant le processus Java souhaité
+                if (line.contains(processId)) {
+                    String[] tokens = line.trim().split("\\s+");
+                    String pid = tokens[0];
+                    String heapDumpCommand = "jcmd " + pid + " GC.heap_dump heapdump_Retwis_benchmark_" + _tag +"_" + _nbUserInit +".hprof";
+
+                    // Exécution de la commande jcmd pour effectuer le heap dump
+                    Process heapDumpProcess = Runtime.getRuntime().exec(heapDumpCommand);
+
+                    // Attente de la fin de l'exécution de la commande
+                    int exitCode = heapDumpProcess.waitFor();
+
+                    if (exitCode == 0) {
+                        System.out.println("Heap dump effectué avec succès !");
+                    } else {
+                        System.out.println("Erreur lors de l'exécution de la commande jcmd.");
+                    }
+
+                    break;
+                }
+            }
+
+            reader.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getProcessId() {
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        return processName.split("@")[0];
     }
 }
