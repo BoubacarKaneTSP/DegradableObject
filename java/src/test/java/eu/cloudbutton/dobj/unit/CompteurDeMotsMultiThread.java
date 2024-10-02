4eab3ba819a8194b1cb3a654140321912d2fbe84
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2024-10-02 19:28:27 +0200

    test word count time

--- java/src/test/java/eu/cloudbutton/dobj/unit/CompteurDeMotsMultiThread.java
+++ java/src/test/java/eu/cloudbutton/dobj/unit/CompteurDeMotsMultiThread.java
@@ -1,8 +1,6 @@
 package eu.cloudbutton.dobj.unit;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,8 +8,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CompteurDeMotsMultiThread {
     public static void main(String[] args) throws IOException {
 
-//        String fileName = "experiences/LoremIpsum.txt";
-        String fileName = "/home/bkane/IdeaProjects/DegradableObject/experiences/LoremIpsum.txt";
+        String fileName = "experiences/LoremIpsum.txt";
+//        String fileName = "/home/bkane/IdeaProjects/DegradableObject/experiences/LoremIpsum.txt";
         int nbThreads = Runtime.getRuntime().availableProcessors();
         ArrayList<StringBuilder> textes = new ArrayList<>();
         for (int i = 0; i < nbThreads; i++)
@@ -26,6 +24,16 @@ public class CompteurDeMotsMultiThread {
             }
         }
 
+//        long totalTime = parallel_conc_obj(textes, nbThreads);
+        long totalTime = parallel_synchronyzed(textes, nbThreads);
+
+        System.out.println("Temps de calcul : " + (double) totalTime / 1_000_000_000 + " seconds");
+
+//        long totalMots = compteurGlobale.values().stream().mapToInt(AtomicInteger::get).sum();
+//        System.out.println("Nombre total de mots : " + totalMots);
+    }
+
+    public static long parallel_conc_obj(ArrayList<StringBuilder> textes, int nbThreads){
         ExecutorService executor = Executors.newFixedThreadPool(nbThreads);
         ArrayList<Runnable> taches = new ArrayList<>();
         Map<String, AtomicInteger> compteurGlobale = new ConcurrentHashMap<>();
@@ -38,13 +46,13 @@ public class CompteurDeMotsMultiThread {
             taches.add(() -> {
                 int taille = mots.length;
                 for (int j = 0; j < taille; j++) {
-                      String mot = mots[j].replaceAll("^[^a-zA-Z-]+|[^a-zA-Z-]+$", "");
-                      if (mot.matches(regex)) {
-                            compteurGlobale.merge(mot, new AtomicInteger(1), (ancien, _) -> {
-                                ancien.incrementAndGet();
-                                return ancien;
-                            });
-                      }
+                    String mot = mots[j].replaceAll("^[^a-zA-Z-]+|[^a-zA-Z-]+$", "");
+                    if (mot.matches(regex)) {
+                        compteurGlobale.merge(mot, new AtomicInteger(1), (ancien, _) -> {
+                            ancien.incrementAndGet();
+                            return ancien;
+                        });
+                    }
                 }
             });
         }
@@ -63,9 +71,53 @@ public class CompteurDeMotsMultiThread {
 
         endTime = System.nanoTime();
         totalTime = endTime - startTime;
-        System.out.println("Temps de calcul : " + (double) totalTime / 1_000_000_000 + " seconds");
 
-        int totalMots = compteurGlobale.values().stream().mapToInt(AtomicInteger::get).sum();
-        System.out.println("Nombre total de mots : " + totalMots);
+        return totalTime;
     }
+
+    public static long parallel_synchronyzed(ArrayList<StringBuilder> textes, int nbThreads){
+        ExecutorService executor = Executors.newFixedThreadPool(nbThreads);
+        ArrayList<Runnable> taches = new ArrayList<>();
+        Map<String, Integer> compteurGlobale = new HashMap<>();
+        String regex = "^[a-zA-Z-]+$";
+
+        for (int i = 0; i < nbThreads; i++) {
+
+            String[] mots = textes.get(i).toString().replaceAll("[^a-zA-Z\\s-]", "").trim().split("\\s+");
+
+            taches.add(() -> {
+                HashMap<String,Integer> compteurLocale = new HashMap();
+
+                int taille = mots.length;
+                for (int j = 0; j < taille; j++) {
+                    String mot = mots[j].replaceAll("^[^a-zA-Z-]+|[^a-zA-Z-]+$", "");
+                    if (mot.matches(regex)) {
+                        compteurLocale.merge(mot, 1, Integer::sum);
+                    }
+                }
+
+                synchronized (compteurGlobale) {
+                    compteurLocale.forEach((m, c) -> compteurGlobale.merge(m, c, Integer::sum));
+                }
+            });
+        }
+
+        long totalTime, endTime, startTime = System.nanoTime();
+
+        for (Runnable task : taches)
+            executor.execute(task);
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        endTime = System.nanoTime();
+        totalTime = endTime - startTime;
+
+        return totalTime;
+    }
+
 }
